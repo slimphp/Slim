2d0fd67ff2c885257a168dd1dce4da6f50bb66ff
Josh Lockhart <joshlockhart@gmail.com>
2010-09-21 20:52:09 -0500

    Adding PHP 4 routing examples enabled by merging changes from j4mie to enable PHP4 compatibility

--- bootstrap.php
+++ bootstrap.php
@@ -1,40 +1,102 @@
 <?php
 
+/*** REQUIRE SLIM ***/
+
 require('slim/Slim.php');
 
-/* INIT SLIM */
+
+/*** INITIALIZE SLIM ***/
 
 Slim::init();
 
-/* CALLBACKS */
 
+/*** CALLBACKS ***/
+
+//Register a "before" callback for PHP >=5.3
 Slim::before(function () {
 	Slim::response()->write('Before!');
 });
 
+//Register a "before" callback for PHP <5.3
+/*
+Slim::before('example_before');
+function example_before() {
+	Slim::response()->write('Before!');
+}
+*/
+
+//Register an "after" callback for PHP >=5.3
 Slim::after(function () {
 	Slim::response()->write('After!');
 });
 
-/* ROUTES */
+//Register an "after" callback for PHP <5.3
+/*
+Slim::after('example_after');
+function example_after() {
+	Slim::response()->write('After!');
+}
+*/
 
+
+/*** ROUTES ***/
+
+//Sample GET route for PHP >=5.3
 Slim::get('/', function () {
 	Slim::render('index.php');
 });
 
+//Sample GET route for PHP <5.3
+/*
+Slim::get('/', 'get_example');
+function get_example() {
+	Slim::render('index.php');
+}
+*/
+
+//Sample POST route for PHP >=5.3
 Slim::post('/post', function () {
 	echo '<br/><br/>Here are the details about your POST request:<br/><br/>';
 	print_r(Slim::request());
 });
 
+//Sample POST route for PHP <5.3
+/*
+Slim::post('/post', 'post_example');
+function post_example() {
+	echo '<br/><br/>Here are the details about your POST request:<br/><br/>';
+	print_r(Slim::request());
+}
+*/
+
+//Sample PUT route for PHP >=5.3
 Slim::put('/put', function () {
 	echo '<br/><br/>Here are the details about your PUT request:<br/><br/>';
 	print_r(Slim::request());
 });
 
+//Sample PUT route for PHP <5.3
+/*
+Slim::put('/put', 'put_example');
+function put_example() {
+	echo '<br/><br/>Here are the details about your PUT request:<br/><br/>';
+	print_r(Slim::request());
+}
+*/
+
+//Sample DELETE route for PHP >=5.3
 Slim::delete('/delete', function () {
 	echo '<br/><br/>Here are the details about your DELETE request:<br/><br/>';
 	print_r(Slim::request());
 });
+
+//Sample DELETE route for PHP <5.3
+/*
+Slim::delete('/delete', 'delete_example');
+function delete_example() {
+	echo '<br/><br/>Here are the details about your DELETE request:<br/><br/>';
+	print_r(Slim::request());
+}
+*/
 
 ?>
