ae89628715fadc844425d6414a36d349a0d6f8e9
Josh Lockhart <joshlockhart@gmail.com>
2010-09-28 12:00:09 -0500

    Changing require to require_once for all dependencies to avoid conflict with unit tests

--- slim/Slim.php
+++ slim/Slim.php
@@ -32,11 +32,11 @@
 
 set_include_path(realpath(dirname(__FILE__)) . PATH_SEPARATOR . get_include_path());
 
-require('Request.php');
-require('Response.php');
-require('Router.php');
-require('Route.php');
-require('View.php');
+require_once('Request.php');
+require_once('Response.php');
+require_once('Router.php');
+require_once('Route.php');
+require_once('View.php');
 
 class Slim {
 	
