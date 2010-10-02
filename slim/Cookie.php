465a4b5cf3a5cf59fd54b423b28abb0690789bfb
Josh Lockhart <joshlockhart@gmail.com>
2010-10-02 23:08:29 +0800

    Adding Cookie class

---
+++ slim/Cookie.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Slim
+ *
+ * A simple PHP framework for PHP 5 or newer
+ *
+ * @author		Josh Lockhart <info@joshlockhart.com>
+ * @link		http://slim.joshlockhart.com
+ * @copyright	2010 Josh Lockhart
+ * 
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+class Cookie {
+
+	public $name;
+	public $value;
+	public $expires;
+	public $path;
+	public $domain;
+	public $secure;
+	public $httponly;
+	
+	public function __construct($name, $value = null, $expires = 0, $path = null, $domain = null, $secure = false, $httponly = false) {
+		$this->name = $name;
+		$this->value = $value;
+		$this->expires = $expires;
+		$this->path = $path;
+		$this->domain = $domain;
+		$this->secure = $secure;
+		$this->httponly = $httponly;
+	}
+	
+}
+
+?>
