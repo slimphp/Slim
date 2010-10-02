08b9bf3526b05593fbf0ebaf3f9c3de7ffccf966
Josh Lockhart <joshlockhart@gmail.com>
2010-10-01 23:11:46 -0400

    Updating Slim::redirect inline docs

--- slim/Slim.php
+++ slim/Slim.php
@@ -475,6 +475,11 @@ class Slim {
     /**
      * Redirect
      *
+	 * This method immediately redirects the client to a new URL. By default,
+	 * this issues a 307 Temporary Redirect. You may also specify a
+	 * 301 Permanent Redirect if you want. This method will automatically
+	 * set the HTTP Location header for you using the URL parameter.
+	 *
      * @param   string                      $url        The destination URL
      * @param   int                         $status     The HTTP redirect status code (Optional)
      * @throws  InvalidArgumentException                If status parameter is not 301 or 307
