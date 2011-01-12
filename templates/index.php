09d58251753de3fc6638e4047c2631e04c183134
Josh Lockhart <joshlockhart@gmail.com>
2011-01-12 00:07:57 -0600

    Simplifying new default app templates

--- templates/index.php
+++ templates/index.php
@@ -1,11 +1,60 @@
-<?php include 'templates/header.php'; ?>
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Slim Micro PHP 5 Framework</title>
+		<style>
+			html,body,div,span,object,iframe,
+			h1,h2,h3,h4,h5,h6,p,blockquote,pre,
+			abbr,address,cite,code,
+			del,dfn,em,img,ins,kbd,q,samp,
+			small,strong,sub,sup,var,
+			b,i,
+			dl,dt,dd,ol,ul,li,
+			fieldset,form,label,legend,
+			table,caption,tbody,tfoot,thead,tr,th,td,
+			article,aside,canvas,details,figcaption,figure,
+			footer,header,hgroup,menu,nav,section,summary,
+			time,mark,audio,video{margin:0;padding:0;border:0;outline:0;font-size:100%;vertical-align:baseline;background:transparent;}
+			body{line-height:1;}
+			article,aside,details,figcaption,figure,
+			footer,header,hgroup,menu,nav,section{display:block;}
+			nav ul{list-style:none;}
+			blockquote,q{quotes:none;}
+			blockquote:before,blockquote:after,
+			q:before,q:after{content:'';content:none;}
+			a{margin:0;padding:0;font-size:100%;vertical-align:baseline;background:transparent;}
+			ins{background-color:#ff9;color:#000;text-decoration:none;}
+			mark{background-color:#ff9;color:#000;font-style:italic;font-weight:bold;}
+			del{text-decoration:line-through;}
+			abbr[title],dfn[title]{border-bottom:1px dotted;cursor:help;}
+			table{border-collapse:collapse;border-spacing:0;}
+			hr{display:block;height:1px;border:0;border-top:1px solid #cccccc;margin:1em 0;padding:0;}
+			input,select{vertical-align:middle;}
+			html{ background: #EDEDED; height: 100%; }
+			body{background:#FFF;margin:0 auto;min-height:100%;padding:0 30px;width:440px;color:#666;font:14px/23px Arial,Verdana,sans-serif;}
+			h1,h2,h3,p,ul,ol,form{margin:0 0 20px 0;}
+			h1{color:#333;font-size:20px;}
+			h2,h3{margin:0;color:#333;font-size:14px;}
+			h3{font-size:12px;font-weight:normal;}
+			ul,ol{list-style-position:inside;color:#999;}
+			ul{list-style-type:square;}
+			code,kbd{background:#EEE;border:1px solid #DDD;border:1px solid #DDD;border-radius:4px;-moz-border-radius:4px;-webkit-border-radius:4px;padding:0 4px;color:#666;font-size:12px;}
+			pre{background:#EEE;border:1px solid #DDD;border-radius:4px;-moz-border-radius:4px;-webkit-border-radius:4px;padding:5px 10px;color:#666;font-size:12px;}
+			pre code{background:transparent;border:none;padding:0;}
+			a{color:#70a23e;}
+			header{padding: 30px 0;text-align:center;}
+		</style>
+	</head>
+	<body>
+		<header>
+			<a href="http://www.slimframework.com"><img src="logo.png" alt="Slim"/></a>
+		</header>
 		<h1>Welcome to Slim!</h1>
 		<p>
 			Congratulations! Your Slim application is running. If this is 
 			your first time using Slim, start with this <a href="http://www.slimframework.com/learn" target="_blank">"Hello World" Tutorial</a>.
 		</p>
-		
 		<section>
 			<h2>Getting Started</h2>
 			<p>Here's some information about this application.</p>
@@ -15,44 +64,14 @@
 				<li>Custom Views for Twig, Smarty, and Mustache are in <code>views/</code></li>
 			</ol>
 		</section>
-		
-		<section>
-			<h2>RESTful Routes</h2>
-			<p>Click the buttons below to test Slim's RESTful routing.</p>
-			<div style="margin-left: 1.5em; font-size: 12px">
-				<h3>Sample POST Route</h3>
-				<p style="margin: 0">Click this button to view an example POST route</p>
-				<form action="post" method="post">
-					<input type="hidden" name="foo" value="bar"/>
-					<input type="submit" value="Send POST"/>
-				</form>
-
-				<h3>Sample PUT Route</h3>
-				<p style="margin: 0">Click this button to view an example PUT route</p>
-				<form action="put" method="post">
-					<input type="hidden" name="foo" value="bar"/>
-					<input type="hidden" name="_METHOD" value="PUT"/>
-					<input type="submit" value="Send PUT"/>
-				</form>
-
-				<h3>Sample DELETE Route</h3>
-				<p style="margin: 0">Click this button to view an example DELETE route</p>
-				<form action="delete" method="post">
-					<input type="hidden" name="foo" value="bar"/>
-					<input type="hidden" name="_METHOD" value="DELETE"/>
-					<input type="submit" value="Send DELETE"/>
-				</form>
-			</div>
-		</section>
-		
 		<section>
 			<h2>Browse the online documentation</h2>
 			<p>Use these links to learn more about the Slim PHP 5 framework.</p>
-			<ul style="margin-bottom: 0">
+			<ul style="margin-bottom: 0; padding-bottom: 30px">
 				<li>Browse the <a href="https://github.com/codeguy/Slim/wiki/Slim-Framework-Documentation" target="_blank">stable version docs</a></li>
 				<li>Browse the <a href="https://github.com/codeguy/Slim/wiki/Documentation" target="_blank">develop version docs</a></li>
 				<li>Follow <a href="http://www.twitter.com/slimphp" target="_blank">@slimphp</a> on Twitter</li>
 			</ul>
 		</section>
-
-<?php include 'templates/footer.php'; ?>
+	</body>
+</html>
--- templates/post.php
+++
@@ -1,8 +0,0 @@
-<?php include 'templates/header.php'; ?>
-
-		<h1>You submitted a POST request.</h1>
-		<p>Here are details about the POST request.</p>
-		<p><a href="/">Return to main page</a></p>
-		<pre><code><?php print_r($request); ?></code></pre>
-
-<?php include 'templates/footer.php'; ?>
