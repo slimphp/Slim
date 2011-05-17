4703ef418ce696c3917c28116f261a43e9846889
Josh Lockhart <joshlockhart@gmail.com>
2011-05-16 19:54:34 -0500

    Add unit test for Slim::getHooks

--- tests/SlimTest.php
+++ tests/SlimTest.php
@@ -1296,6 +1296,8 @@ class SlimTest extends PHPUnit_Extensions_OutputTestCase {
         $callable2 = function () { echo "bar"; };
         $app->hook('test.hook.one', $callable1); //default is 10
         $app->hook('test.hook.one', $callable2, 8);
+        $hooks = $app->getHooks();
+        $this->assertEquals(7, count($hooks)); //6 default, 1 custom
         $app->applyHook('test.hook.one');
     }
 
