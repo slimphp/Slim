c4d3475e98f778b426ca7ea7931e27a4c28f2e36
Josh Lockhart <joshlockhart@gmail.com>
2011-12-24 12:33:43 -0500

    Add session cookie middleware with unit tests

--- Slim/Middleware/SessionCookie.php
+++ Slim/Middleware/SessionCookie.php
@@ -1,23 +1,157 @@
 <?php
+/**
+ * Slim - a micro PHP 5 framework
+ *
+ * @author      Josh Lockhart <info@slimframework.com>
+ * @copyright   2011 Josh Lockhart
+ * @link        http://www.slimframework.com
+ * @license     http://www.slimframework.com/license
+ * @version     2.0.0
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Session Cookie
+ *
+ * This class provides an HTTP cookie storage mechanism
+ * for session data. This class avoids using a PHP session
+ * and instead serializes/unserializes the $_SESSION global
+ * variable to/from an HTTP cookie.
+ *
+ * If a secret key is provided with this middleware, the HTTP
+ * cookie will be checked for integrity to ensure the client-side
+ * cookie is not changed.
+ *
+ * You should NEVER store sensitive data in a client-side cookie
+ * in any format, encrypted or not. If you need to store sensitive
+ * user information in a session, you should rely on PHP's native
+ * session implementation, or use other middleware to store
+ * session data in a database or alternative server-side cache.
+ *
+ * Because this class stores serialized session data in an HTTP cookie,
+ * you are inherently limtied to 4 Kb. If you attempt to store
+ * more than this amount, serialization will fail.
+ *
+ * @package    Slim
+ * @author     Josh Lockhart
+ * @since      2.0.0
+ */
 class Slim_Middleware_SessionCookie {
-    private $app;
-    private $settings;
+    /**
+     * @var Slim
+     */
+    protected $app;
 
-    public function __construct($app, $settings = array()) {
+    /**
+     * @var array
+     */
+    protected $settings;
+
+    /**
+     * Constructor
+     *
+     * @param   Slim $app
+     * @param   array $settings
+     * @return  void
+     */
+    public function __construct( $app, $settings = array() ) {
         $this->app = $app;
-        $this->settings = $settings;
-    }
-
-    public function call($env) {
-        if ( session_id() === '' ) {
-            session_cache_limiter(false);
-            session_start();
-            list($status, $headers, $body) = $this->app->call($env);
-            //TODO: Persist session with given settings
-            return array($status, $headers, $body);
-        } else {
-            $this->app->call($env);
+        $this->settings = array_merge(array(
+            'expires' => '20 minutes',
+            'path' => '/',
+            'domain' => null,
+            'secure' => false,
+            'httponly' => false,
+            'name' => 'slim_session',
+            'secret' => 'CHANGE_ME',
+            'cipher' => MCRYPT_RIJNDAEL_256,
+            'cipher_mode' => MCRYPT_MODE_CBC
+        ), $settings);
+        if ( is_string($this->settings['expires']) ) {
+            $this->settings['expires'] = strtotime($this->settings['expires']);
         }
     }
+
+    /**
+     * Call
+     * @param   array $env
+     * @return  array[status, header, body]
+     */
+    public function call( &$env ) {
+        $this->loadSession($env);
+        list($status, $header, $body) = $this->app->call($env);
+        return $this->saveSession($env, $status, $header, $body);
+    }
+
+    /**
+     * Load session
+     * @param   array $env
+     * @return  void
+     */
+    protected function loadSession( &$env ) {
+        $req = new Slim_Http_Request($env);
+        $value = Slim_Http_Util::decodeSecureCookie(
+            $req->cookies($this->settings['name']),
+            $this->settings['secret'],
+            $this->settings['cipher'],
+            $this->settings['cipher_mode']
+        );
+        if ( $value ) {
+            $_SESSION = unserialize($value);
+        } else {
+            $_SESSION = array();
+        }
+    }
+
+    /**
+     * Save session
+     * @param   int     $status
+     * @param   array   $header
+     * @param   string  $body
+     * @return  array[status, header, body]
+     */
+    protected function saveSession( &$env, $status, $header, $body ) {
+        $r = new Slim_Http_Response($body, $status, $header);
+        $value = Slim_Http_Util::encodeSecureCookie(
+            serialize($_SESSION),
+            $this->settings['expires'],
+            $this->settings['secret'],
+            $this->settings['cipher'],
+            $this->settings['cipher_mode']
+        );
+        if ( strlen($value) > 4096 ) {
+            fwrite($env['slim.errors'], 'WARNING! Slim_Middleware_SessionCookie data size is larger than 4KB. Content save failed.');
+        } else {
+            $r->setCookie(
+                $this->settings['name'],
+                $value,
+                $this->settings['expires'],
+                $this->settings['path'],
+                $this->settings['domain'],
+                $this->settings['secure'],
+                $this->settings['httponly']
+            );
+        }
+        return $r->finalize();
+    }
 }
-?>
---
+++ tests/Middleware/SessionCookieTest.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Slim - a micro PHP 5 framework
+ *
+ * @author      Josh Lockhart <info@joshlockhart.com>
+ * @copyright   2011 Josh Lockhart
+ * @link        http://www.slimframework.com
+ * @license     http://www.slimframework.com/license
+ * @version     1.5.0
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
+
+require_once 'Slim/Http/Util.php';
+require_once 'Slim/Http/Request.php';
+require_once 'Slim/Http/Response.php';
+require_once 'Slim/Http/Headers.php';
+require_once 'Slim/Middleware/SessionCookie.php';
+
+class CustomAppSet {
+    function call( &$env ) {
+        $_SESSION['foo'] = 'bar';
+        return array(200, array(), 'Test');
+    }
+}
+
+class CustomAppGet {
+    function call( &$env ) {
+        if ( isset($_SESSION['foo']) ) {
+            return array(200, array(), $_SESSION['foo']);
+        } else {
+            return array(200, array(), 'Not Set');
+        }
+    }
+}
+
+class SessionCookieTest extends PHPUnit_Extensions_OutputTestCase {
+    public function setUp() {
+        $_SESSION = array();
+    }
+
+    /**
+     * Test session cookie is set and constructed correctly
+     *
+     * We test for two things:
+     * 1) That the HTTP cookie is added to the `Set-Cookie:` response header;
+     * 2) That the HTTP cookie is constructed in the expected format;
+     */
+    public function testSessionCookieIsCreatedAndEncrypted() {
+        $env = array(
+            'REQUEST_METHOD' => 'GET',
+            'REMOTE_ADDR' => '127.0.0.1',
+            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
+            'PATH_INFO' => '/bar', //<-- Virtual
+            'QUERY_STRING' => 'one=1&two=2&three=3',
+            'SERVER_NAME' => 'slim',
+            'SERVER_PORT' => 80,
+            'slim.url_scheme' => 'http',
+            'slim.input' => '',
+            'slim.errors' => fopen('php://stderr', 'w')
+        );
+        $app = new CustomAppSet();
+        $mw = new Slim_Middleware_SessionCookie($app, array(
+            'expires' => '10 years'
+        ));
+        list($status, $header, $body) = $mw->call($env);
+        $this->assertEquals(1, preg_match('@^slim_session=.+|.+|.+; expires=@', $header['Set-Cookie']));
+    }
+
+    /**
+     * Test $_SESSION is populated from HTTP cookie
+     *
+     * The HTTP cookie in this test was created using the previous test; the encrypted cookie contains
+     * the serialized array ['foo' => 'bar']. The middleware secret, cipher, and cipher mode are assumed
+     * to be the default values.
+     */
+    public function testSessionIsPopulatedFromCookie() {
+        $env = array(
+            'REQUEST_METHOD' => 'GET',
+            'REMOTE_ADDR' => '127.0.0.1',
+            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
+            'PATH_INFO' => '/bar', //<-- Virtual
+            'QUERY_STRING' => 'one=1&two=2&three=3',
+            'HTTP_COOKIE' => 'slim_session=1639490378%7CqWbI5R%2Bf%2B%2F1KfHQQ9cANqEEdK5aNhf%2FQy2WX%2FCFOG5Y%3D%7Ce207c55544e1f7889a357ab39700f9cbb3836ea3',
+            'SERVER_NAME' => 'slim',
+            'SERVER_PORT' => 80,
+            'slim.url_scheme' => 'http',
+            'slim.input' => '',
+            'slim.errors' => fopen('php://stderr', 'w')
+        );
+        $app = new CustomAppGet();
+        $mw = new Slim_Middleware_SessionCookie($app);
+        list($status, $header, $body) = $mw->call($env);
+        $this->assertEquals(array('foo' => 'bar'), $_SESSION);
+    }
+
+    /**
+     * Test $_SESSION is populated as empty array if no HTTP cookie
+     */
+    public function testSessionIsPopulatedAsEmptyIfNoCookie() {
+        $env = array(
+            'REQUEST_METHOD' => 'GET',
+            'REMOTE_ADDR' => '127.0.0.1',
+            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
+            'PATH_INFO' => '/bar', //<-- Virtual
+            'QUERY_STRING' => 'one=1&two=2&three=3',
+            'SERVER_NAME' => 'slim',
+            'SERVER_PORT' => 80,
+            'slim.url_scheme' => 'http',
+            'slim.input' => '',
+            'slim.errors' => fopen('php://stderr', 'w')
+        );
+        $app = new CustomAppGet();
+        $mw = new Slim_Middleware_SessionCookie($app);
+        list($status, $header, $body) = $mw->call($env);
+        $this->assertEquals(array(), $_SESSION);
+    }
+}
