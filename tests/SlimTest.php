d0351cf0774bbbaff8fa97e28b8c90f3e7cfcbc4
Hans-Peter Oeri <hp@oeri.ch>
2012-02-03 12:26:19 +0100

    TEST FIX for optional autoloader

--- tests/SlimTest.php
+++ tests/SlimTest.php
@@ -33,6 +33,7 @@
 set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 
 require_once 'Slim/Slim.php';
+require_once 'Slim/View.php';
 require_once 'Slim/Middleware/Interface.php';
 require_once 'Slim/Middleware/Flash.php';
 
