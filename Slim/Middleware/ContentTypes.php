03c6493919136b3908a6cf22973e0f7ec1ea8764
Benjamin Johnson <benpjohnson@gmail.com>
2012-03-25 17:03:32 +0100

    fix handling of Content-Type with charset for content parsing

--- Slim/Middleware/ContentTypes.php
+++ Slim/Middleware/ContentTypes.php
@@ -76,8 +76,14 @@ class Slim_Middleware_ContentTypes implements Slim_Middleware_Interface {
      */
     public function call( &$env ) {
         if ( isset($env['CONTENT_TYPE']) ) {
+            if ( strpos($env['CONTENT_TYPE'],';') !== false ) {
+                list($contentType, $charset) = explode(';', $env['CONTENT_TYPE']);
+            }
+            else {
+                $contentType = $env['CONTENT_TYPE'];
+            }
             $env['slim.input_original'] = $env['slim.input'];
-            $env['slim.input'] = $this->parse($env['slim.input'], $env['CONTENT_TYPE']);
+            $env['slim.input'] = $this->parse($env['slim.input'], $contentType);
         }
         return $this->app->call($env);
     }
