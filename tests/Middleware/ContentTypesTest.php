171306b3952ef630195d516fca2fe214d16845b0
Josh Lockhart <joshlockhart@gmail.com>
2012-04-03 19:52:26 -0400

    Update ContentTypes middleware unit tests with new middleware architecture

--- tests/Middleware/ContentTypesTest.php
+++ tests/Middleware/ContentTypesTest.php
@@ -32,140 +32,107 @@
 
 set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 
-require_once 'Slim/Middleware/Interface.php';
+require_once 'Slim/Slim.php';
 require_once 'Slim/Middleware/ContentTypes.php';
 
-class CustomApp {
-    function call( &$env ) {
-        return $env['slim.input'];
-    }
-}
-
 class ContentTypesTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        ob_start();
+    }
+
+    public function tearDown() {
+        ob_end_clean();
+    }
+
     /**
      * Test parses JSON
      */
     public function testParsesJson() {
-        $env = array(
+        Slim_Environment::mock(array(
             'REQUEST_METHOD' => 'POST',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
-            'PATH_INFO' => '/bar', //<-- Virtual
-            'QUERY_STRING' => 'one=1&two=2&three=3',
-            'SERVER_NAME' => 'slim',
-            'SERVER_PORT' => 80,
             'CONTENT_TYPE' => 'application/json',
             'CONENT_LENGTH' => 13,
-            'slim.url_scheme' => 'http',
-            'slim.input' => '{"foo":"bar"}',
-            'slim.errors' => fopen('php://stderr', 'w')
-        );
-        $app = new CustomApp();
-        $mw = new Slim_Middleware_ContentTypes($app);
-        $result = $mw->call($env);
-        $this->assertTrue(is_array($result));
-        $this->assertEquals('bar', $result['foo']);
+            'slim.input' => '{"foo":"bar"}'
+        ));
+        $s = new Slim();
+        $s->add(new Slim_Middleware_ContentTypes());
+        $s->run();
+        $body = $s->request()->getBody();
+        $this->assertTrue(is_array($body));
+        $this->assertEquals('bar', $body['foo']);
     }
 
     /**
      * Test ignores JSON with errors
      */
     public function testParsesJsonWithError() {
-        $env = array(
+        Slim_Environment::mock(array(
             'REQUEST_METHOD' => 'POST',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
-            'PATH_INFO' => '/bar', //<-- Virtual
-            'QUERY_STRING' => 'one=1&two=2&three=3',
-            'SERVER_NAME' => 'slim',
-            'SERVER_PORT' => 80,
             'CONTENT_TYPE' => 'application/json',
             'CONENT_LENGTH' => 12,
-            'slim.url_scheme' => 'http',
-            'slim.input' => '{"foo":"bar"',
-            'slim.errors' => fopen('php://stderr', 'w')
-        );
-        $app = new CustomApp();
-        $mw = new Slim_Middleware_ContentTypes($app);
-        $result = $mw->call($env);
-        $this->assertTrue(is_string($result));
-        $this->assertEquals('{"foo":"bar"', $result);
+            'slim.input' => '{"foo":"bar"' //<-- This should be incorrect!
+        ));
+        $s = new Slim();
+        $s->add(new Slim_Middleware_ContentTypes());
+        $s->run();
+        $body = $s->request()->getBody();
+        $this->assertTrue(is_string($body));
+        $this->assertEquals('{"foo":"bar"', $body);
     }
 
     /**
      * Test parses XML
      */
     public function testParsesXml() {
-        $env = array(
+        Slim_Environment::mock(array(
             'REQUEST_METHOD' => 'POST',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
-            'PATH_INFO' => '/bar', //<-- Virtual
-            'QUERY_STRING' => 'one=1&two=2&three=3',
-            'SERVER_NAME' => 'slim',
-            'SERVER_PORT' => 80,
             'CONTENT_TYPE' => 'application/xml',
             'CONENT_LENGTH' => 68,
-            'slim.url_scheme' => 'http',
-            'slim.input' => '<books><book><id>1</id><author>Clive Cussler</author></book></books>',
-            'slim.errors' => fopen('php://stderr', 'w')
-        );
-        $app = new CustomApp();
-        $mw = new Slim_Middleware_ContentTypes($app);
-        $result = $mw->call($env);
-        $this->assertInstanceOf('SimpleXMLElement', $result);
-        $this->assertEquals('Clive Cussler', (string)$result->book->author);
+            'slim.input' => '<books><book><id>1</id><author>Clive Cussler</author></book></books>'
+        ));
+        $s = new Slim();
+        $s->add(new Slim_Middleware_ContentTypes());
+        $s->run();
+        $body = $s->request()->getBody();
+        $this->assertInstanceOf('SimpleXMLElement', $body);
+        $this->assertEquals('Clive Cussler', (string)$body->book->author);
     }
 
     /**
      * Test ignores XML with errors
      */
     public function testParsesXmlWithError() {
-        $env = array(
+        Slim_Environment::mock(array(
             'REQUEST_METHOD' => 'POST',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
-            'PATH_INFO' => '/bar', //<-- Virtual
-            'QUERY_STRING' => 'one=1&two=2&three=3',
-            'SERVER_NAME' => 'slim',
-            'SERVER_PORT' => 80,
             'CONTENT_TYPE' => 'application/xml',
             'CONENT_LENGTH' => 68,
-            'slim.url_scheme' => 'http',
-            'slim.input' => '<books><book><id>1</id><author>Clive Cussler</book></books>',
-            'slim.errors' => fopen('php://stderr', 'w')
-        );
-        $app = new CustomApp();
-        $mw = new Slim_Middleware_ContentTypes($app);
-        $result = $mw->call($env);
-        $this->assertTrue(is_string($result));
-        $this->assertEquals('<books><book><id>1</id><author>Clive Cussler</book></books>', $result);
+            'slim.input' => '<books><book><id>1</id><author>Clive Cussler</book></books>' //<-- This should be incorrect!
+        ));
+        $s = new Slim();
+        $s->add(new Slim_Middleware_ContentTypes());
+        $s->run();
+        $body = $s->request()->getBody();
+        $this->assertTrue(is_string($body));
+        $this->assertEquals('<books><book><id>1</id><author>Clive Cussler</book></books>', $body);
     }
 
     /**
      * Test parses CSV
      */
     public function testParsesCsv() {
-        $env = array(
+        Slim_Environment::mock(array(
             'REQUEST_METHOD' => 'POST',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => '/foo/index.php', //<-- Physical
-            'PATH_INFO' => '/bar', //<-- Virtual
-            'QUERY_STRING' => 'one=1&two=2&three=3',
-            'SERVER_NAME' => 'slim',
-            'SERVER_PORT' => 80,
             'CONTENT_TYPE' => 'text/csv',
             'CONENT_LENGTH' => 44,
-            'slim.url_scheme' => 'http',
-            'slim.input' => "John,Doe,000-111-2222\nJane,Doe,111-222-3333",
-            'slim.errors' => fopen('php://stderr', 'w')
-        );
-        $app = new CustomApp();
-        $mw = new Slim_Middleware_ContentTypes($app);
-        $result = $mw->call($env);
-        $this->assertTrue(is_array($result));
-        $this->assertEquals(2, count($result));
-        $this->assertEquals('000-111-2222', $result[0][2]);
-        $this->assertEquals('Doe', $result[1][1]);
+            'slim.input' => "John,Doe,000-111-2222\nJane,Doe,111-222-3333"
+        ));
+        $s = new Slim();
+        $s->add(new Slim_Middleware_ContentTypes());
+        $s->run();
+        $body = $s->request()->getBody();
+        $this->assertTrue(is_array($body));
+        $this->assertEquals(2, count($body));
+        $this->assertEquals('000-111-2222', $body[0][2]);
+        $this->assertEquals('Doe', $body[1][1]);
     }
 }
