b8d26e7161523fba86d8a50848644307098ee53d
= <info@joshlockhart.com>
2012-07-31 08:12:27 -0400

    Bump version number in file headers

--- Slim/Route.php
+++ Slim/Route.php
@@ -6,7 +6,7 @@
  * @copyright   2011 Josh Lockhart
  * @link        http://www.slimframework.com
  * @license     http://www.slimframework.com/license
- * @version     1.6.4
+ * @version     1.6.5
  * @package     Slim
  *
  * MIT LICENSE
