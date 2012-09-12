3c4e5cef48daea95ae5d9649cd117307da92cca7
Brian Nesbitt <brian@nesbot.com>
2012-09-12 13:03:14 -0300

    Fix autoload to use current directory
    Fix autoload to use current directory

--- Slim/Slim.php
+++ Slim/Slim.php
@@ -124,13 +124,21 @@ class Slim
      */
     public static function autoload($className)
     {
+        $thisClass = str_replace(__NAMESPACE__.'\\', '', __CLASS__);
+
+        $baseDir = __DIR__;
+   
+        if (substr($baseDir, -strlen($thisClass)) === $thisClass) {
+            $baseDir = substr($baseDir, 0, -strlen($thisClass));
+        }
+
         $className = ltrim($className, '\\');
-        $fileName  = '';
+        $fileName  = $baseDir;
         $namespace = '';
         if ($lastNsPos = strripos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
             $className = substr($className, $lastNsPos + 1);
-            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
