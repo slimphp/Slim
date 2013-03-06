220c1120603bd86d73fc8ea4fa6a3d42fc21347e
WooDzu <woodzu123+github.com@gmail.com>
2013-03-06 01:27:01 +0000

    Add basic support for php-cli
    Allows running a Slim app from comman line.
    Converts command line params into Slim GET routes
    php index.php hello WooDzu -> $app->get('/hello/:who', ... ) ->
    /hello/WooDzu

--- Slim/Environment.php
+++ Slim/Environment.php
@@ -117,49 +117,70 @@ class Environment implements \ArrayAccess, \IteratorAggregate
         if ($settings) {
             $this->properties = $settings;
         } else {
-            $env = array();
 
-            //The HTTP request method
-            $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
+            // Virtual environment for php-cli (ignore for phpunit)
+            if( PHP_SAPI == 'cli' && $_SERVER['SERVER_NAME'] != 'slim' )
+            {
 
-            //The IP
-            $env['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
+                $argv = $GLOBALS['argv'];
 
-            /**
-             * Application paths
-             *
-             * This derives two paths: SCRIPT_NAME and PATH_INFO. The SCRIPT_NAME
-             * is the real, physical path to the application, be it in the root
-             * directory or a subdirectory of the public document root. The PATH_INFO is the
-             * virtual path to the requested resource within the application context.
-             *
-             * With htaccess, the SCRIPT_NAME will be an absolute path (without file name);
-             * if not using htaccess, it will also include the file name. If it is "/",
-             * it is set to an empty string (since it cannot have a trailing slash).
-             *
-             * The PATH_INFO will be an absolute path with a leading slash; this will be
-             * used for application routing.
-             */
-            if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) === 0) {
-                $env['SCRIPT_NAME'] = $_SERVER['SCRIPT_NAME']; //Without URL rewrite
-            } else {
-                $env['SCRIPT_NAME'] = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']) ); //With URL rewrite
+                array_shift( $argv );
+
+                // Convert $argv parameters to PATH string
+                $env = self::mock(array(
+                    'SCRIPT_NAME'   => $_SERVER['SCRIPT_NAME'],
+                    'PATH_INFO'     => '/'.implode( '/', $argv )
+                ));
+
             }
-            $env['PATH_INFO'] = substr_replace($_SERVER['REQUEST_URI'], '', 0, strlen($env['SCRIPT_NAME']));
-            if (strpos($env['PATH_INFO'], '?') !== false) {
-                $env['PATH_INFO'] = substr_replace($env['PATH_INFO'], '', strpos($env['PATH_INFO'], '?')); //query string is not removed automatically
+            else
+            {
+                $env = array();
+
+                //The HTTP request method
+                $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
+
+                //The IP
+                $env['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
+
+                //Name of server host that is running the script
+                $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
+
+                //Number of server port that is running the script
+                $env['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
+
+                /**
+                 * Application paths
+                 *
+                 * This derives two paths: SCRIPT_NAME and PATH_INFO. The SCRIPT_NAME
+                 * is the real, physical path to the application, be it in the root
+                 * directory or a subdirectory of the public document root. The PATH_INFO is the
+                 * virtual path to the requested resource within the application context.
+                 *
+                 * With htaccess, the SCRIPT_NAME will be an absolute path (without file name);
+                 * if not using htaccess, it will also include the file name. If it is "/",
+                 * it is set to an empty string (since it cannot have a trailing slash).
+                 *
+                 * The PATH_INFO will be an absolute path with a leading slash; this will be
+                 * used for application routing.
+                 */
+
+                if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) === 0) {
+                    $env['SCRIPT_NAME'] = $_SERVER['SCRIPT_NAME']; //Without URL rewrite
+                } else {
+                    $env['SCRIPT_NAME'] = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']) ); //With URL rewrite
+                }
+                $env['PATH_INFO'] = substr_replace($_SERVER['REQUEST_URI'], '', 0, strlen($env['SCRIPT_NAME']));
+            
+                if (strpos($env['PATH_INFO'], '?') !== false) {
+                    $env['PATH_INFO'] = substr_replace($env['PATH_INFO'], '', strpos($env['PATH_INFO'], '?')); //query string is not removed automatically
+                }
+                $env['SCRIPT_NAME'] = rtrim($env['SCRIPT_NAME'], '/');
+                $env['PATH_INFO'] = '/' . ltrim($env['PATH_INFO'], '/');
+
+                //The portion of the request URI following the '?'
+                $env['QUERY_STRING'] = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
             }
-            $env['SCRIPT_NAME'] = rtrim($env['SCRIPT_NAME'], '/');
-            $env['PATH_INFO'] = '/' . ltrim($env['PATH_INFO'], '/');
-
-            //The portion of the request URI following the '?'
-            $env['QUERY_STRING'] = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
-
-            //Name of server host that is running the script
-            $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
-
-            //Number of server port that is running the script
-            $env['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
 
             //HTTP request headers
             $specialHeaders = array('CONTENT_TYPE', 'CONTENT_LENGTH', 'PHP_AUTH_USER', 'PHP_AUTH_PW', 'PHP_AUTH_DIGEST', 'AUTH_TYPE');
