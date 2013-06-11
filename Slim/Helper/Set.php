24c056ef4bdb01bf8b15207265b422f5856f60c0
Paul Croker <paul@zomble.com.au>
2013-06-11 18:10:46 +1000

    Add remaining magic. Also remembered PSR-2.

--- Slim/Helper/Set.php
+++ Slim/Helper/Set.php
@@ -139,19 +139,29 @@ class Set implements \ArrayAccess, \Countable, \IteratorAggregate
         unset($this->data[$this->normalizeKey($key)]);
     }
 
-	/**
-	 * Property Overloading
-	 */
+    /**
+     * Property Overloading
+     */
 
-	public function __get($key)
-	{
-		return $this->get($key);
-	}
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
 
-	public function __isset($key)
-	{
-		return $this->has($key);
-	}
+    public function __set($key, $value)
+    {
+        $this->set($key, $value);
+    }
+
+    public function __isset($key)
+    {
+        return $this->has($key);
+    }
+
+    public function __unset($key)
+    {
+        return $this->remove($key);
+    }
 
     /**
      * Clear all values
