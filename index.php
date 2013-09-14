865439d8161ddf7c991864ef4362bc4392e90e2f
= <info@joshlockhart.com>
2013-09-14 14:23:51 -0400

    Update index.php
    Remove old \Slim\Slim.php class

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require 'Slim/Autoloader.php';
  * your Slim application now by passing an associative array
  * of setting names and values into the application constructor.
  */
-$app = new \Slim\Slim();
+$app = new \Slim\App();
 
 /**
  * Step 3: Define the Slim application routes
