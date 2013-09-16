5c3466ea0cb662b5ce2b4accbde1a12cf01fd332
= <info@joshlockhart.com>
2013-09-16 18:24:56 -0400

    Update \Slim\View inline docs

--- Slim/View.php
+++ Slim/View.php
@@ -35,24 +35,42 @@ namespace Slim;
 /**
  * View
  *
- * The view is responsible for rendering a template. The view
- * should subclass \Slim\View and implement this interface:
+ * This class is responsible for fetching and rendering a template with
+ * a given set of data. Although the `\Slim\View` class is itself
+ * capable of rendering PHP templates, it is highly recommended that you
+ * subclass `\Slim\View` for use with popular PHP templating libraries
+ * such as Twig, Smarty, or Mustache.
  *
- * public render(string $template);
+ * If you do choose to create a subclass of `\Slim\View`, the subclass
+ * MUST override the `render` method with this exact signature:
  *
- * This method should render the specified template and return
- * the resultant string.
+ *     public render(string $template);
+ *
+ * The `render` method MUST return the rendered output for the template
+ * identified by the `$template` argument. The `$template` argument will
+ * contain the template file pathname *relative to* the templates base
+ * directory for the current view instance.
+ *
+ * The `Slim-Views` repository contains pre-made custom views for
+ * Twig and Smarty, two of the most popular PHP templating libraries.
+ *
+ * See: https://github.com/codeguy/Slim-Views
+ *
+ * Also, `\Slim\View` extends `\Slim\Container` so
+ * that you may use the convenient `\Slim\Container` interface just
+ * as you do with other Slim application data sets (e.g. HTTP headers,
+ * HTTP cookies, etc.)
  *
  * @package Slim
  * @author  Josh Lockhart
  * @since   1.0.0
+ * @see     \Slim\Container
  */
 class View extends \Slim\Container
 {
     /**
      * Constructor
-     * @param string                     $templateDirectory Path to template directory
-     * @throws \InvalidArgumentException If directory is unreadable or not a directory
+     * @param string $templateDirectory Path to template directory
      */
     public function __construct($templateDirectory = null)
     {
@@ -72,9 +90,9 @@ class View extends \Slim\Container
     }
 
     /**
-     * Return the contents of a rendered template file
-     * @var    string $template The template pathname, relative to the template base directory
-     * @return string           The rendered template
+     * Return the content of a rendered template file
+     * @var    string   $template   Pathname of template file relative to templates directory
+     * @return string               The rendered template
      */
     public function fetch($template)
     {
@@ -83,9 +101,9 @@ class View extends \Slim\Container
 
     /**
      * Render a template file
-     * @var    string $template     The template pathname, relative to the template base directory
-     * @return string               The rendered template
-     * @throws \RuntimeException    If resolved template pathname is not a valid file
+     * @var    string               $template       Pathname of template file relative to templates directory
+     * @return string                               The rendered template
+     * @throws \RuntimeException                    If resolved template pathname is not a valid file
      */
     protected function render($template)
     {
