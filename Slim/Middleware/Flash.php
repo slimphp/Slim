3dfc647235d03407c1a01c038c146e19ca09bd78
= <info@joshlockhart.com>
2013-10-08 13:54:23 -0400

    Add hhvm configuration example to README
    Fix inline doc versions in a few class files

--- Slim/Middleware/Flash.php
+++ Slim/Middleware/Flash.php
@@ -6,7 +6,7 @@
  * @copyright   2011 Josh Lockhart
  * @link        http://www.slimframework.com
  * @license     http://www.slimframework.com/license
- * @version     2.2.0
+ * @version     2.3.3
  * @package     Slim
  *
  * MIT LICENSE
