34052d6df37eefc60a26cca31764ea860d4dbb58
= <info@joshlockhart.com>
2013-10-11 13:37:28 -0400

    Add Collection class and tests

---
+++ Slim/Collection.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Slim - a micro PHP 5 framework
+ *
+ * @author      Josh Lockhart <info@slimframework.com>
+ * @copyright   2011 Josh Lockhart
+ * @link        http://www.slimframework.com
+ * @license     http://www.slimframework.com/license
+ * @version     2.3.3
+ * @package     Slim
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+namespace Slim;
+
+class Collection implements \Countable, \IteratorAggregate
+{
+    /**
+     * Key-value array of data
+     * @var array
+     */
+    protected $data = array();
+
+    /**
+     * Constructor
+     * @param array $items Pre-populate collection with this key-value array
+     */
+    public function __construct(array $items = array())
+    {
+        $this->replace($items);
+    }
+
+    /**
+     * Set data key to value
+     * @param string $key   The data key
+     * @param mixed  $value The data value
+     */
+    public function set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * Get data value with key
+     * @param  string $key     The data key
+     * @param  mixed  $default The value to return if data key does not exist
+     * @return mixed           The data value, or the default value
+     */
+    public function get($key, $default = null)
+    {
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Add data to set
+     * @param array $items Key-value array of data to append to this set
+     */
+    public function replace($items)
+    {
+        foreach ($items as $key => $value) {
+            $this->set($key, $value);
+        }
+    }
+
+    /**
+     * Fetch set data
+     * @return array This set's key-value data array
+     */
+    public function all()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Fetch set data keys
+     * @return array This set's key-value data array keys
+     */
+    public function keys()
+    {
+        return array_keys($this->data);
+    }
+
+    /**
+     * Does this set contain a key?
+     * @param  string  $key The data key
+     * @return boolean
+     */
+    public function has($key)
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    /**
+     * Remove value with key from this set
+     * @param  string $key The data key
+     */
+    public function remove($key)
+    {
+        unset($this->data[$key]);
+    }
+
+    /**
+     * Clear all values
+     */
+    public function clear()
+    {
+        $this->data = array();
+    }
+
+    /**
+     * Encrypt set
+     * @param  \Slim\Crypt $crypt
+     * @return void
+     */
+    public function encrypt(\Slim\Crypt $crypt)
+    {
+        foreach ($this as $elementName => $elementValue) {
+            $this->set($elementName, $crypt->encrypt($elementValue));
+        }
+    }
+
+    /**
+     * Decrypt set
+     * @param  \Slim\Crypt $crypt
+     * @return void
+     */
+    public function decrypt(\Slim\Crypt $crypt)
+    {
+        foreach ($this as $elementName => $elementValue) {
+            $this->set($elementName, $crypt->decrypt($elementValue));
+        }
+    }
+
+    /**
+     * Get number of items in collection
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Get collection iterator
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->data);
+    }
+}
