slimphp/Slim

commit a35767746009ff818e6375b72f23a2cf7de5f1a4

Add HHVM compatibility

tests/EnvironmentTest.php
@@ -43,6 +43,8 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
+        $_SERVER['DOCUMENT_ROOT'] = '/var/www';
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/foo/index.php';
         $_SERVER['SERVER_NAME'] = 'slim';
         $_SERVER['SERVER_PORT'] = '80';
         $_SERVER['SCRIPT_NAME'] = '/foo/index.php';
@@ -106,6 +108,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testParsesPathsWithoutUrlRewriteInRootDirectory()
     {
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/index.php';
         $_SERVER['REQUEST_URI'] = '/index.php/bar/xyz';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
         $env = \Slim\Environment::getInstance(true);
@@ -123,6 +126,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testParsesPathsWithoutUrlRewriteInRootDirectoryForAppRootUri()
     {
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/index.php';
         $_SERVER['REQUEST_URI'] = '/index.php';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
         unset($_SERVER['PATH_INFO']);
@@ -157,6 +161,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testParsesPathsWithUrlRewriteInRootDirectory()
     {
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/index.php';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
         $_SERVER['REQUEST_URI'] = '/bar/xyz';
         unset($_SERVER['PATH_INFO']);
@@ -175,6 +180,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testParsesPathsWithUrlRewriteInRootDirectoryForAppRootUri()
     {
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/index.php';
         $_SERVER['REQUEST_URI'] = '/';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
         unset($_SERVER['PATH_INFO']);
@@ -224,6 +230,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testPathInfoRetainsUrlEncodedCharacters()
     {
+        $_SERVER['SCRIPT_FILENAME'] = '/var/www/index.php';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
         $_SERVER['REQUEST_URI'] = '/foo/%23bar'; //<-- URL-encoded "#bar"
         $env = \Slim\Environment::getInstance(true);

Slim/Environment.php
@@ -126,35 +126,21 @@ class Environment implements \ArrayAccess, \IteratorAggregate
             //The IP
             $env['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
 
-            /**
-             * Application paths
-             *
-             * This derives two paths: SCRIPT_NAME and PATH_INFO. The SCRIPT_NAME
-             * is the real, physical path to the application, be it in the root
-             * directory or a subdirectory of the public document root. The PATH_INFO is the
-             * virtual path to the requested resource within the application context.
-             *
-             * With htaccess, the SCRIPT_NAME will be an absolute path (without file name);
-             * if not using htaccess, it will also include the file name. If it is "/",
-             * it is set to an empty string (since it cannot have a trailing slash).
-             *
-             * The PATH_INFO will be an absolute path with a leading slash; this will be
-             * used for application routing.
-             */
-            if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) === 0) {
-                $env['SCRIPT_NAME'] = $_SERVER['SCRIPT_NAME']; //Without URL rewrite
+            // Root URI (physical path) and resource URI (virtual path)
+            $scriptName = str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']); // <-- "/physical/index.php"
+            $requestUri = $_SERVER['REQUEST_URI']; // <-- "/physical/index.php/virtual?abc=123" or "/physical/virtual?abc=123"
+            $queryString = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : ''; // <-- "abc=123"
+            if (strpos($requestUri, $scriptName) === false) {
+                // With rewriting
+                $env['SCRIPT_NAME'] = str_replace('/' . basename($scriptName), '', $scriptName);
             } else {
-                $env['SCRIPT_NAME'] = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])); //With URL rewrite
+                // Without rewriting
+                $env['SCRIPT_NAME'] = $scriptName;
             }
-            $env['PATH_INFO'] = substr_replace($_SERVER['REQUEST_URI'], '', 0, strlen($env['SCRIPT_NAME']));
-            if (strpos($env['PATH_INFO'], '?') !== false) {
-                $env['PATH_INFO'] = substr_replace($env['PATH_INFO'], '', strpos($env['PATH_INFO'], '?')); //query string is not removed automatically
-            }
-            $env['SCRIPT_NAME'] = rtrim($env['SCRIPT_NAME'], '/');
-            $env['PATH_INFO'] = '/' . ltrim($env['PATH_INFO'], '/');
+            $env['PATH_INFO'] = '/' . ltrim(str_replace(array($env['SCRIPT_NAME'], '?' . $queryString), '', $requestUri), '/');
 
-            //The portion of the request URI following the '?'
-            $env['QUERY_STRING'] = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
+            // Query string (without leading "?")
+            $env['QUERY_STRING'] = $queryString;
 
             //Name of server host that is running the script
             $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];

commit 19b9c29907cffe9cbf0e9895cfd60dde7d700abb

Tests for bug introduced with commit a35767746009ff818e6375b72f23a2cf7de5f1a4

tests/EnvironmentTest.php
@@ -153,6 +153,42 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test parses script name and path info when route and installation subdirectory starts with the same name
+     *
+     * Pre-conditions:
+     * URL Rewrite enabled;
+     * App installed in subdirectory;
+     * Route and installation subdirectory starts with the same name
+     */
+    public function testParsesPathsWithCommonPrefixForSubdirectoryAndRouteNameWithUrlRewriteInSubdirectory()
+    {
+        $_SERVER['SCRIPT_NAME'] = '/foo/index.php';
+        $_SERVER['REQUEST_URI'] = '/foo/foobar';
+        unset($_SERVER['PATH_INFO']);
+        $env = \Slim\Environment::getInstance(true);
+        $this->assertEquals('/foobar', $env['PATH_INFO']);
+        $this->assertEquals('/foo', $env['SCRIPT_NAME']);
+    }
+
+    /**
+     * Test parses script name and path info when route and installation subdirectory have the same name
+     *
+     * Pre-conditions:
+     * URL Rewrite enabled;
+     * App installed in subdirectory;
+     * Route and installation subdirectory have with the same name
+     */
+    public function testParsesPathsWithCommonSubdirectoryAndRouteNameWithUrlRewriteInSubdirectory()
+    {
+        $_SERVER['SCRIPT_NAME'] = '/foo/index.php';
+        $_SERVER['REQUEST_URI'] = '/foo/foo';
+        unset($_SERVER['PATH_INFO']);
+        $env = \Slim\Environment::getInstance(true);
+        $this->assertEquals('/foo', $env['PATH_INFO']);
+        $this->assertEquals('/foo', $env['SCRIPT_NAME']);
+    }
+
+    /**
      * Test parses script name and path info
      *
      * Pre-conditions: