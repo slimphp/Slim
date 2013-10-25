90d4d696a530b682b2a6afe5a1a9937ea130e6b6
Ilan Frumer <ilanfrumer@gmail.com>
2013-10-25 13:21:55 +0300

    bindTo only if php >= 5.4

--- Slim/Slim.php
+++ Slim/Slim.php
@@ -426,7 +426,7 @@ class Slim
     {
         $pattern = array_shift($args);
         $callable = array_pop($args);
-        $callable = $callable->bindTo($this);
+        if(PHP_MINOR_VERSION >= 4) $callable = $callable->bindTo($this);
         $route = new \Slim\Route($pattern, $callable);
         $this->router->map($route);
         if (count($args) > 0) {
@@ -535,7 +535,7 @@ class Slim
         $args = func_get_args();
         $pattern = array_shift($args);
         $callable = array_pop($args);
-        $callable = $callable->bindTo($this);
+        if(PHP_MINOR_VERSION >= 4) $callable = $callable->bindTo($this);
         $this->router->pushGroup($pattern, $args);
         if (is_callable($callable)) {
             call_user_func($callable);
