1e6454a9bf2eb29a7c145d91a6edbabb5b40fa62
= <info@joshlockhart.com>
2013-10-30 22:01:04 -0400

    Bump version

--- Slim/Slim.php
+++ Slim/Slim.php
@@ -6,7 +6,7 @@
  * @copyright   2011 Josh Lockhart
  * @link        http://www.slimframework.com
  * @license     http://www.slimframework.com/license
- * @version     2.3.4
+ * @version     2.3.5
  * @package     Slim
  *
  * MIT LICENSE
@@ -49,7 +49,7 @@ class Slim
     /**
      * @const string
      */
-    const VERSION = '2.3.4';
+    const VERSION = '2.3.5';
 
     /**
      * @var \Slim\Helper\Set
