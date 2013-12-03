0d8b354427899626783b78e639811976add520c4
Josh Lockhart <info@joshlockhart.com>
2013-12-03 10:30:12 -0500

    Refactor Environment

--- Slim/Environment.php
+++ Slim/Environment.php
@@ -35,63 +35,10 @@ namespace Slim;
 /**
  * Environment
  *
- * This class determines the environmental variables used by
- * the Slim application. It removes the Slim application's dependency
- * on the $_SERVER superglobal and lets the Slim application
+ * This class determines the environment variables used by
+ * the Slim application and lets the Slim application
  * depend on a controlled set of environmental variables that may be
  * mocked, if necessary.
- *
- * The set of environmental variables mirrors the Rack (Ruby) specification
- * as closely as possible. The environmental variables are:
- *
- *     1. SERVER_PROTOCOL
- *     The HTTP request protocol (e.g. "HTTP/1.1")
- *
- *     2. REQUEST_METHOD
- *     The HTTP request method (e.g. "GET", "POST", "PUT", "DELETE")
- *
- *     3. SCRIPT_NAME
- *     The initial portion of the request URI’s “path” that corresponds to the
- *     physical directory in which the Slim application is installed — so that
- *     the application knows its virtual “location”. This may be an empty string
- *     if the application is installed in the top-level of the public document
- *     root directory. This will never have a trailing slash.
- *
- *     4. PATH_INFO
- *     The remaining portion of the request URI’s “path” that determines the
- *     “virtual” location of the HTTP request’s target resource within the Slim
- *     application context. This will always have a leading slash; it may or
- *     may not have a trailing slash.
- *
- *     5. QUERY_STRING
- *     The part of the HTTP request’s URI after, but not including, the “?”.
- *     This is required but may be an empty string.
- *
- *     6. SERVER_NAME
- *     This is the `Host:` HTTP header. When combined with SCRIPT_NAME and PATH_INFO,
- *     this can be used to create a fully qualified URL to an application resource.
- *     However, if HTTP_HOST is present, that should be used instead of this.
- *     This is required and may never be an empty string.
- *
- *     7. SERVER_PORT
- *     When combined with SCRIPT_NAME and PATH_INFO, this can be used to create a
- *     fully qualified URL to any application resource. This is required and
- *     may never be an empty string.
- *
- *     8. HTTP_*
- *     Variables matching the HTTP request headers sent by the client. The existence
- *     of these variables correspond with those sent in the current HTTP request.
- *     These variables will retain the "HTTP_" prefix.
- *
- *     9. REMOTE_ADDR
- *     The IP address from which the user is viewing the current page.
- *
- *     10. slim.url_scheme
- *     The HTTP request scheme. Will be “http” or “https”.
- *
- *     11. slim.input
- *     The raw HTTP request body. If the HTTP request body is empty (e.g. with a GET request),
- *     this will be an empty string.
  *
  * @package Slim
  * @author  Josh Lockhart
@@ -100,107 +47,37 @@ namespace Slim;
 class Environment extends \Slim\Collection
 {
     /**
-     * The raw HTTP request body, readable only once from `php://input`
-     * @var string
+     * Create environment from globals
+     * @return \Slim\Environment
      */
-    protected static $requestBody;
+    public static function createFromGlobals()
+    {
+        return new static($_SERVER);
+    }
 
     /**
-     * Mock environment
-     *
-     * Use this method to create a set of mock environmental variables
-     * instead of relying on the $_SERVER superglobal. This is useful
-     * for unit testing.
-     *
-     * @param  array             $userSettings
+     * Create environment from mock data
      * @return \Slim\Environment
-     * @api
      */
-    public static function mock(array $userSettings = array())
+    public static function mock(array $userData = array())
     {
-        return new static(array_merge(array(
+        $data = array_merge(array(
             'SERVER_PROTOCOL'      => 'HTTP/1.1',
             'REQUEST_METHOD'       => 'GET',
             'SCRIPT_NAME'          => '',
-            'PATH_INFO'            => '/',
+            'REQUEST_URI'          => '',
             'QUERY_STRING'         => '',
             'SERVER_NAME'          => 'localhost',
             'SERVER_PORT'          => 80,
+            'HTTP_HOST'            => 'localhost',
             'HTTP_ACCEPT'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.8',
             'HTTP_ACCEPT_CHARSET'  => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
             'HTTP_USER_AGENT'      => 'Slim Framework',
             'REMOTE_ADDR'          => '127.0.0.1',
-            'slim.url_scheme'      => 'http',
-            'slim.input'           => ''
-        ), $userSettings));
-    }
+            'REQUEST_TIME'         => time()
+        ), $userData);
 
-    /**
-     * Constructor
-     * @param  array $settings Environmental variables. Leave blank to use $_SERVER superglobal
-     * @api
-     */
-    public function __construct(array $settings = null)
-    {
-        if (is_null($settings)) {
-            $settings = array();
-
-            // Request protocol (e.g. "HTTP/1.1")
-            $settings['SERVER_PROTOCOL'] = $_SERVER['SERVER_PROTOCOL'];
-
-            // Request method
-            $settings['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
-
-	        // Server params
-            $scriptName = $_SERVER['SCRIPT_NAME']; // <-- "/foo/index.php"
-            $requestUri = $_SERVER['REQUEST_URI']; // <-- "/foo/bar?test=abc" or "/foo/index.php/bar?test=abc"
-            $queryString = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : ''; // <-- "test=abc" or ""
-
-            // Physical path
-            if (strpos($requestUri, $scriptName) !== false) {
-                $physicalPath = $scriptName; // <-- Without rewriting
-            } else {
-                $physicalPath = str_replace('\\', '', dirname($scriptName)); // <-- With rewriting
-            }
-            $settings['SCRIPT_NAME'] = rtrim($physicalPath, '/'); // <-- Remove trailing slashes
-
-            // Virtual path
-            $settings['PATH_INFO'] = substr_replace($requestUri, '', 0, strlen($physicalPath)); // <-- Remove physical path
-            $settings['PATH_INFO'] = str_replace('?' . $queryString, '', $settings['PATH_INFO']); // <-- Remove query string
-            $settings['PATH_INFO'] = '/' . ltrim($settings['PATH_INFO'], '/'); // <-- Ensure leading slash
-
-            // Query string (without leading "?")
-            $settings['QUERY_STRING'] = $queryString;
-
-            $settings['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
-
-            // Server port
-            $settings['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
-
-            // Request headers (with "HTTP_" prefix)
-            $headers = \Slim\Http\Headers::find($_SERVER);
-            foreach ($headers as $key => $value) {
-                $settings[$key] = $value;
-            }
-
-            // IP address
-            $settings['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
-
-            // Request scheme ("http" or "https")
-            $settings['slim.url_scheme'] = empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off' ? 'http' : 'https';
-
-            // Request body (readable one time only; not available for multipart/form-data requests)
-            if (is_null(static::$requestBody)) {
-                $body = file_get_contents('php://input');
-                if ($body === false) {
-                    $body = '';
-                }
-                static::$requestBody = $body;
-            }
-            $settings['slim.input'] = static::$requestBody;
-        }
-
-        parent::__construct($settings);
+        return new static($data);
     }
 }
--- tests/EnvironmentTest.php
+++ tests/EnvironmentTest.php
@@ -33,13 +33,8 @@
 class EnvironmentTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Default server settings assume the Slim app is installed
-     * in a subdirectory `foo/` directly beneath the public document
-     * root directory; URL rewrite is disabled; requested app
-     * resource is GET `/bar/xyz` with three query params.
-     *
-     * These only provide a common baseline for the following
-     * tests; tests are free to override these values.
+     * Server settings for the default HTTP request
+     * used by this script's tests.
      */
     public function setUp()
     {
@@ -53,311 +48,31 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
         $_SERVER['QUERY_STRING'] = 'one=1&two=2&three=3';
         $_SERVER['HTTPS'] = '';
         $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-        unset($_SERVER['CONTENT_TYPE'], $_SERVER['CONTENT_LENGTH']);
     }
 
     /**
-     * Test mock environment
-     *
-     * This should return the custom values where specified
-     * and the default values otherwise.
+     * Test environment from globals
      */
-    public function testMockEnvironment()
+    public function testEnvironmentFromGlobals()
+    {
+        $env = \Slim\Environment::createFromGlobals();
+
+        $this->assertInstanceOf('\Slim\Environment', $env);
+        $this->assertEquals($env->all(), $_SERVER);
+    }
+
+    /**
+     * Test environment from mock data
+     */
+    public function testEnvironmentFromMockData()
     {
         $env = \Slim\Environment::mock(array(
-            'REQUEST_METHOD' => 'PUT'
+            'SCRIPT_NAME' => '/foo/bar/index.php',
+            'REQUEST_URI' => '/foo/bar?abc=123'
         ));
+
         $this->assertInstanceOf('\Slim\Environment', $env);
-        $this->assertEquals('PUT', $env->get('REQUEST_METHOD'));
-        $this->assertEquals(80, $env->get('SERVER_PORT'));
-        $this->assertNull($env->get('foo'));
-    }
-
-    /**
-     * Test sets HTTP method
-     */
-    public function testSetsHttpMethod()
-    {
-        $env = new \Slim\Environment();
-        $this->assertEquals('GET', $env->get('REQUEST_METHOD'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite is disabled;
-     * App installed in subdirectory;
-     */
-    public function testParsesPathsWithoutUrlRewriteInSubdirectory()
-    {
-        $env = new \Slim\Environment();
-        $this->assertEquals('/bar/xyz', $env->get('PATH_INFO'));
-        $this->assertEquals('/foo/index.php', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite is disabled;
-     * App installed in root directory;
-     */
-    public function testParsesPathsWithoutUrlRewriteInRootDirectory()
-    {
-	    $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['REQUEST_URI'] = '/index.php/bar/xyz';
-        $env = new \Slim\Environment();
-        $this->assertEquals('/bar/xyz', $env->get('PATH_INFO'));
-        $this->assertEquals('/index.php', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite disabled;
-     * App installed in root directory;
-     * Requested resource is "/";
-     */
-    public function testParsesPathsWithoutUrlRewriteInRootDirectoryForAppRootUri()
-    {
-	    $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['REQUEST_URI'] = '/index.php';
-        unset($_SERVER['PATH_INFO']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('/', $env->get('PATH_INFO'));
-        $this->assertEquals('/index.php', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite enabled;
-     * App installed in subdirectory;
-     */
-    public function testParsesPathsWithUrlRewriteInSubdirectory()
-    {
-        $_SERVER['SCRIPT_NAME'] = '/foo/index.php';
-        $_SERVER['REQUEST_URI'] = '/foo/bar/xyz';
-        unset($_SERVER['PATH_INFO']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('/bar/xyz', $env->get('PATH_INFO'));
-        $this->assertEquals('/foo', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite enabled;
-     * App installed in root directory;
-     */
-    public function testParsesPathsWithUrlRewriteInRootDirectory()
-    {
-        $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['REQUEST_URI'] = '/bar/xyz';
-        unset($_SERVER['PATH_INFO']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('/bar/xyz', $env->get('PATH_INFO'));
-        $this->assertEquals('', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses script name and path info
-     *
-     * Pre-conditions:
-     * URL Rewrite enabled;
-     * App installed in root directory;
-     * Requested resource is "/"
-     */
-    public function testParsesPathsWithUrlRewriteInRootDirectoryForAppRootUri()
-    {
-        $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['REQUEST_URI'] = '/';
-        unset($_SERVER['PATH_INFO']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('/', $env->get('PATH_INFO'));
-        $this->assertEquals('', $env->get('SCRIPT_NAME'));
-    }
-
-    /**
-     * Test parses query string
-     *
-     * Pre-conditions:
-     * $_SERVER['QUERY_STRING'] exists and is not empty;
-     */
-    public function testParsesQueryString()
-    {
-        $env = new \Slim\Environment();
-        $this->assertEquals('one=1&two=2&three=3', $env->get('QUERY_STRING'));
-    }
-
-    /**
-     * Test removes query string from PATH_INFO when using URL Rewrite
-     *
-     * Pre-conditions:
-     * $_SERVER['QUERY_STRING'] exists and is not empty;
-     * URL Rewrite enabled;
-     */
-    public function testRemovesQueryStringFromPathInfo()
-    {
-        $_SERVER['SCRIPT_NAME'] = '/foo/index.php';
-        $_SERVER['REQUEST_URI'] = '/foo/bar/xyz?one=1&two=2&three=3';
-        unset($_SERVER['PATH_INFO']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('/bar/xyz', $env->get('PATH_INFO'));
-    }
-
-    /**
-     * Test environment's PATH_INFO retains URL encoded characters (e.g. #)
-     *
-     * In earlier version, \Slim\Environment would use PATH_INFO instead
-     * of REQUEST_URI to determine the root URI and resource URI.
-     * Unfortunately, the server would URL decode the PATH_INFO string
-     * before it was handed to PHP. This prevented certain URL-encoded
-     * characters like the octothorpe from being delivered correctly to
-     * the Slim application environment. This test ensures the
-     * REQUEST_URI is used instead and parsed as expected.
-     */
-    public function testPathInfoRetainsUrlEncodedCharacters()
-    {
-        $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['REQUEST_URI'] = '/foo/%23bar'; //<-- URL-encoded "#bar"
-        $env = new \Slim\Environment();
-        $this->assertEquals('/foo/%23bar', $env->get('PATH_INFO'));
-    }
-
-    /**
-     * Test parses query string
-     *
-     * Pre-conditions:
-     * $_SERVER['QUERY_STRING'] does not exist;
-     */
-    public function testParsesQueryStringThatDoesNotExist()
-    {
-        unset($_SERVER['QUERY_STRING']);
-        $env = new \Slim\Environment();
-        $this->assertEquals('', $env->get('QUERY_STRING'));
-    }
-
-    /**
-     * Test SERVER_NAME is not empty
-     */
-    public function testServerNameIsNotEmpty()
-    {
-        $env = new \Slim\Environment();
-        $serverName = $env->get('SERVER_NAME');
-        $this->assertTrue(!is_null($serverName));
-    }
-
-    /**
-     * Test SERVER_PORT is not empty
-     */
-    public function testServerPortIsNotEmpty()
-    {
-        $env = new \Slim\Environment();
-        $serverPort = $env->get('SERVER_PORT');
-        $this->assertTrue(!is_null($serverPort));
-    }
-
-    /**
-     * Test unsets HTTP_CONTENT_TYPE and HTTP_CONTENT_LENGTH
-     *
-     * Pre-conditions:
-     * HTTP_CONTENT_TYPE is sent with HTTP request;
-     * HTTP_CONTENT_LENGTH is sent with HTTP request;
-     */
-    public function testUnsetsContentTypeAndContentLength()
-    {
-        $_SERVER['HTTP_CONTENT_TYPE'] = 'text/csv';
-        $_SERVER['HTTP_CONTENT_LENGTH'] = 150;
-        $env = new \Slim\Environment();
-        $this->assertNull($env->get('HTTP_CONTENT_TYPE'));
-        $this->assertNull($env->get('HTTP_CONTENT_LENGTH'));
-    }
-
-    /**
-     * Test sets special request headers if not empty
-     *
-     * Pre-conditions:
-     * CONTENT_TYPE, CONTENT_LENGTH, X_REQUESTED_WITH are sent in client HTTP request;
-     * CONTENT_TYPE, CONTENT_LENGTH, X_REQUESTED_WITH are not empty;
-     */
-    public function testSetsSpecialHeaders()
-    {
-        $_SERVER['CONTENT_TYPE'] = 'text/csv';
-        $_SERVER['CONTENT_LENGTH'] = '100';
-        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XmlHttpRequest';
-        $env = new \Slim\Environment();
-        $this->assertEquals('text/csv', $env->get('CONTENT_TYPE'));
-        $this->assertEquals('100', $env->get('CONTENT_LENGTH'));
-        $this->assertEquals('XmlHttpRequest', $env->get('HTTP_X_REQUESTED_WITH'));
-    }
-
-    /**
-     * Tests X-HTTP-Method-Override is allowed through unmolested.
-     *
-     * Pre-conditions:
-     * X_HTTP_METHOD_OVERRIDE is sent in client HTTP request;
-     * X_HTTP_METHOD_OVERRIDE is not empty;
-     */
-    public function testSetsHttpMethodOverrideHeader() {
-        $_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'] = 'DELETE';
-        $env = new \Slim\Environment();
-        $this->assertEquals('DELETE', $env->get('HTTP_X_HTTP_METHOD_OVERRIDE'));
-    }
-
-    /**
-     * Test detects HTTPS
-     *
-     * Pre-conditions:
-     * $_SERVER['HTTPS'] is set to a non-empty value;
-     */
-    public function testHttps()
-    {
-        $_SERVER['HTTPS'] = 1;
-        $env = new \Slim\Environment();
-        $this->assertEquals('https', $env->get('slim.url_scheme'));
-    }
-
-    /**
-     * Test detects not HTTPS
-     *
-     * Pre-conditions:
-     * $_SERVER['HTTPS'] is set to an empty value;
-     */
-    public function testNotHttps()
-    {
-        $_SERVER['HTTPS'] = '';
-        $env = new \Slim\Environment();
-        $this->assertEquals('http', $env->get('slim.url_scheme'));
-    }
-
-    /**
-     * Test detects not HTTPS on IIS
-     *
-     * Pre-conditions:
-     * $_SERVER['HTTPS'] is set to "off";
-     */
-    public function testNotHttpsIIS()
-    {
-        $_SERVER['HTTPS'] = 'off';
-        $env = new \Slim\Environment();
-        $this->assertEquals('http', $env->get('slim.url_scheme'));
-    }
-
-    /**
-     * Test input is an empty string (and not false)
-     *
-     * Pre-conditions:
-     * Input at php://input may only be read once; subsequent attempts
-     * will return `false`; in which case, use an empty string.
-     */
-    public function testInputIsEmptyString()
-    {
-        $env = new \Slim\Environment();
-        $this->assertEquals('', $env->get('slim.input'));
+        $this->assertEquals($env->get('SCRIPT_NAME'), '/foo/bar/index.php');
+        $this->assertEquals($env->get('REQUEST_URI'), '/foo/bar?abc=123');
     }
 }
