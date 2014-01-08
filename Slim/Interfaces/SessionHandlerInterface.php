ca937279c31574508c539389c56808185d20bfce
Charlie Schliesser <charlie.schliesser@gmail.com>
2014-01-08 12:03:11 -0600

    Added missing semicolon in SessionHandlerInterface namespace declaration.

--- Slim/Interfaces/SessionHandlerInterface.php
+++ Slim/Interfaces/SessionHandlerInterface.php
@@ -30,7 +30,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-namespace Slim\Interfaces
+namespace Slim\Interfaces;
 
 /**
  * Session Handler Interface
