0e32f9036f6e2035c1852aec5a78d8b767a1705c
hakre <hanskrentel@yahoo.de>
2014-06-22 17:13:57 +0200

    only cast object to string if there is context #840
    this is the bespoken dirty fix

--- Slim/Log.php
+++ Slim/Log.php
@@ -306,8 +306,8 @@ class Log
         if (!isset(self::$levels[$level])) {
             throw new \InvalidArgumentException('Invalid log level supplied to function');
         } else if ($this->enabled && $this->writer && $level <= $this->level) {
-            $message = (string)$object;
-            if (count($context) > 0) {
+            $message = $object;
+            if ($context) {
                 if (isset($context['exception']) && $context['exception'] instanceof \Exception) {
                     $message .= ' - ' . $context['exception'];
                     unset($context['exception']);
