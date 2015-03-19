6efda1a95f4079eed19d8a5e72c5f222b18e147c
Garrett Morris <stuckinabox@live.com>
2015-03-19 12:32:18 +0000

    good grief the formatting

--- Slim/Http/Request.php
+++ Slim/Http/Request.php
@@ -97,12 +97,12 @@ class Request implements RequestInterface
      */
     protected $body;
 	
-	/**
-	 * The requests uploaded files or empty array
-	 *
-	 * @var array
-	 */
-	protected $files;
+    /**
+     * The requests uploaded files or empty array
+     *
+     * @var array
+     */
+    protected $files;
 
     /**
      * The request body parsed (if possible) into a PHP array or object
