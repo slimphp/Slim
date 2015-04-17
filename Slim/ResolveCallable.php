6683c2fe011941b88f3ed2d006c5892478212486
Samuel Laulhau <zouliloua@gmail.com>
2015-04-17 17:47:49 +0200

    wrap callable resolution

--- Slim/ResolveCallable.php
+++ Slim/ResolveCallable.php
@@ -36,32 +36,15 @@ trait ResolveCallable
      */
     protected function resolveCallable($callable)
     {
-        if (is_string($callable) && strpos($callable, ':') !== false && preg_match('!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!', $callable, $matches)) {
-            // $callable is a class:method string, so wrap it into a closure, retriving the class from Pimple if registered there
-
-            if ((! $this instanceof Container) && (! $this->container instanceof Container)) {
+        if (is_string($callable) && strpos($callable, ':')) {            
+            if($this instanceof Container) {
+                $container = $this;
+            } elseif($this->container instanceof Container) {
+                $container = $this->container;
+            } else {
                 throw new \RuntimeException('Cannot resolve callable string');
-            }
-
-            $class = $matches[1];
-            $method = $matches[2];
-            $callable = function() use ($class, $method) {
-                static $obj = null;
-                if ($obj === null) {
-                    if (isset($this[$class])) {
-                        $obj = $this[$class];
-                    } else {
-                        if (!class_exists($class)) {
-                            throw new \RuntimeException('Route callable class does not exist');
-                        }
-                        $obj = new $class;
-                    }
-                    if (!is_callable([$obj, $method])) {
-                        throw new \RuntimeException('Route callable method does not exist');
-                    }
-                }
-                return call_user_func_array(array($obj, $method), func_get_args());
-            };
+            }            
+            return new CallableResolver($callable, $container);
         }
 
         return $callable;
