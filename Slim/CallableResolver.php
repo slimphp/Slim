909d26f8fd9caf9b99bf9ca8bae6a3339ccdfc36
Samuel Laulhau <zouliloua@gmail.com>
2015-04-21 22:49:20 +0200

    add test

--- Slim/CallableResolver.php
+++ Slim/CallableResolver.php
@@ -21,20 +21,28 @@ class CallableResolver
     
     public function resolve()
     {
-        preg_match('!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!', $this->toResolve, $matches);
-        $class = $matches[1];
-        $method = $matches[2];
-        
-        if (isset($this->container[$class])) {
-            $this->resolved = [$this->container[$class], $method];
-        } else {
-            if (!class_exists($class)) {
-                throw new \RuntimeException('Route callable class does not exist');
+        // check it's callable
+        if(is_callable($this->toResolve)) {
+            $this->resolved = $this->toResolve;
+            
+        // check for slim callable as "class:method"
+        } elseif (is_string($this->toResolve)) {
+            $callable_pattern = '!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
+            preg_match($callable_pattern, $this->toResolve, $matches);
+            $class = $matches[1];
+            $method = $matches[2];
+            
+            if (isset($this->container[$class])) {
+                $this->resolved = [$this->container[$class], $method];
+            } else {
+                if (!class_exists($class)) {
+                    throw new \RuntimeException('Route callable class does not exist');
+                }
+                $this->resolved = [new $class, $method];
             }
-            $this->resolved = [new $class, $method];
-        }
-        if (!is_callable($this->resolved)) {
-            throw new \RuntimeException('Route callable method does not exist');
+            if (!is_callable($this->resolved)) {
+                throw new \RuntimeException('Route callable method does not exist');
+            }
         }
     }
     
