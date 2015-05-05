d80f8d0fbc2c6ff665a334a04a2cc4b91dae2f11
maks feltrin <pine3ree@gmail.com>
2015-05-05 20:23:53 +0200

    Update RouterInterface.php

--- Slim/Interfaces/RouterInterface.php
+++ Slim/Interfaces/RouterInterface.php
@@ -55,10 +55,11 @@ interface RouterInterface
      *
      * @param string $name Route name
      * @param array  $data Route URI segments replacement data
+     * @param array  $queryParams optional query parameters 
      *
      * @return string
      * @throws \RuntimeException         If named route does not exist
      * @throws \InvalidArgumentException If required data not provided
      */
-    public function urlFor($name, $data = []);
+    public function urlFor($name, array $data = [], array $queryParams = []);
 }
