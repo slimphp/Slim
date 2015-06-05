52d7b011bec2a5db962ffc255e4253b5e42bd154
Rob Allen <rob@akrabat.com>
2015-06-05 08:50:59 +0100

    Add docblocks to CallableResolver and CallableResolverInterface

--- Slim/CallableResolver.php
+++ Slim/CallableResolver.php
@@ -5,29 +5,64 @@ namespace Slim;
 use Slim\Interfaces\CallableResolverInterface;
 use Interop\Container\ContainerInterface;
 
+/**
+ * This class resolves a string of the format 'class:method' into a closure
+ * that can be disatched. It is itself invokable as it lazily resolves the string
+ * when it is invoked.
+ */
 final class CallableResolver implements CallableResolverInterface
 {
-    
+    /**
+     * @var ContainerInterface
+     */
     protected $container;
     
+    /**
+     * @var string
+     */
     protected $toResolve;
-    
+
+    /**
+     * @var callable
+     */
     protected $resolved;
     
+    /**
+     * @param ContainerInterface $container
+     * @param string             $toResolve
+     */
     public function __construct(ContainerInterface $container, $toResolve = null)
     {
         $this->toResolve = $toResolve;
         $this->container = $container;
     }
     
+    
+    /**
+     * Receive a string that is to be resolved to a callable
+     *
+     * @param  string $toResolve
+     *
+     * @return void
+     */
     public function setToResolve($toResolve)
     {
         $this->toResolve = $toResolve;
     }
     
+    /**
+     * Resolve toResolve into a closure that that the router can dispatch.
+     *
+     * If toResolve is of the format 'class:method', then try to extract 'class'
+     * from the container otherwise instantiate it and then dispatch 'method'.
+     *
+     * @param  string $callable
+     *
+     * @return \Closure
+     */
     private function resolve()
     {
-        // check it's callable
+        // if it's callable, then it's already resolved
         if (is_callable($this->toResolve)) {
             $this->resolved = $this->toResolve;
             
@@ -55,6 +90,11 @@ final class CallableResolver implements CallableResolverInterface
         }
     }
     
+    /**
+     * Invoke the resolved callable.
+     *
+     * @return \Psr\Http\MessageResponseInterface
+     */
     public function __invoke()
     {
         if (!isset($this->resolved)) {
--- Slim/Interfaces/CallableResolverInterface.php
+++ Slim/Interfaces/CallableResolverInterface.php
@@ -5,6 +5,20 @@ namespace Slim\Interfaces;
 
 interface CallableResolverInterface
 {
+
+    /**
+     * Receive a string that is to be resolved to a callable
+     *
+     * @param  string $toResolve
+     *
+     * @return void
+     */
     public function setToResolve($toResolve);
+
+    /**
+     * Invoke the resolved callable.
+     *
+     * @return \Psr\Http\MessageResponseInterface
+     */
     public function __invoke();
 }
