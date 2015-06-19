dcae25390032411a18f48e34ad24dab5e15c2802
Rob Allen <rob@akrabat.com>
2015-06-19 07:52:07 +0100

    Update urlFor() to support optional parameters
    This is done by using the FastRoute parser to determine the segments and parameters
    for each possible URI that can be generated for this named route. We test the most
    specific first and work out way back to the least specific, so that we can handle
    optional parameters.
    
    Closes #1289.

--- Slim/Router.php
+++ Slim/Router.php
@@ -31,6 +31,13 @@ use Slim\Route;
 class Router extends RouteCollector implements RouterInterface
 {
     /**
+     * Parser
+     *
+     * @var \FastRoute\RouteParser
+     */
+    private $routeParser;
+
+    /**
      * Routes
      *
      * @var Route[]
@@ -62,6 +69,7 @@ class Router extends RouteCollector implements RouterInterface
         $parser = $parser ? $parser : new StdParser;
         $generator = $generator ? $generator : new GroupCountBasedGenerator;
         parent::__construct($parser, $generator);
+        $this->routeParser = $parser;
     }
 
     /**
@@ -181,14 +189,44 @@ class Router extends RouteCollector implements RouterInterface
         $route = $this->namedRoutes[$name];
         $pattern = $route->getPattern();
 
-        $url = preg_replace_callback('/{([^}]+)}/', function ($match) use ($data) {
-            $segmentName = explode(':', $match[1])[0];
-            if (!isset($data[$segmentName])) {
-                throw new InvalidArgumentException('Missing data for URL segment: ' . $segmentName);
-            }
+        $routeDatas = ($this->routeParser->parse($pattern));
+        // $routeDatas is an array of all possible routes that can be made. There is
+        // one routedata for each optional parameter plus one for no optional parameters.
+        //
+        // The most specific is last, so we look for that first.
+        $routeDatas = array_reverse($routeDatas);
 
-            return $data[$segmentName];
-        }, $pattern);
+        $segments = [];
+        foreach ($routeDatas as $routeData) {
+            foreach ($routeData as $item) {
+                if (is_string($item)) {
+                    // this segment is a static string
+                    $segments[] = $item;
+                    continue;
+                }
+
+                // This segment has a parameter: first element is the name
+                if (!array_key_exists($item[0], $data)) {
+                    // we don't have a data element for this segment: cancel
+                    // testing this routeData item, so that we can try a less
+                    // specific routeData item.
+                    $segments = [];
+                    $segmentName = $item[0];
+                    break;
+                }
+                $segments[] = $data[$item[0]];
+            }
+            if (!empty($segments)) {
+                // we found all the parameters for this route data, no need to check
+                // less specific ones
+                break;
+            }
+        }
+
+        if (empty($segments)) {
+            throw new InvalidArgumentException('Missing data for URL segment: ' . $segmentName);
+        }
+        $url = implode('', $segments);
 
         if ($queryParams) {
             $url .= '?' . http_build_query($queryParams);
