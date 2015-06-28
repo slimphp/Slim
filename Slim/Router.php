2db013e9d6fd9e5861081b77facb1c34a78deba6
Rob Allen <rob@akrabat.com>
2015-06-28 19:23:13 +0100

    Add urlFor() as a deprecated method

--- Slim/Router.php
+++ Slim/Router.php
@@ -235,6 +235,25 @@ class Router extends RouteCollector implements RouterInterface
     }
 
     /**
+     * Build the path for a named route.
+     *
+     * This method is deprecated. Use pathFor() from now on.
+     *
+     * @param string $name        Route name
+     * @param array  $data        Named argument replacement data
+     * @param array  $queryParams Optional query string parameters
+     *
+     * @return string
+     * @throws \RuntimeException         If named route does not exist
+     * @throws \InvalidArgumentException If required data not provided
+     */
+    public function urlFor($name, array $data = [], array $queryParams = [])
+    {
+        trigger_error('urlFor() is deprecated. Use pathFor() instead.', E_USER_DEPRECATED);
+        return $this->pathFor($name, $data, $queryParams);
+    }
+
+    /**
      * Build index of named routes
      */
     protected function buildNameIndex()
