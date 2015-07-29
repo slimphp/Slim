e07f7b424cf3a31bac24038a61bff12a8dababa1
Michel Hunziker <info@michelhunziker.com>
2015-07-29 17:05:09 +0200

    Optimize test class loading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,16 @@
 <?php
-set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 
 // Set timezone
 date_default_timezone_set('America/New_York');
 
-// Enable Composer autoloader
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-//Register non-Slim autoloader
-function customAutoLoader($class)
-{
-    $file = dirname(__FILE__) . '/' . $class . '.php';
-    if (file_exists($file)) {
-        require $file;
-    } else {
-        return;
-    }
-}
-spl_autoload_register('customAutoLoader');
-
 // Prevent session cookies
 ini_set('session.use_cookies', 0);
+
+
+
+// Enable Composer autoloader
+/** @var \Composer\Autoload\ClassLoader $autoloader */
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Register test classes
+$autoloader->addPsr4('Slim\Tests\\', __DIR__);
