5d619f9f07e12dacb9f84e0348c98cce2adc06dd
Rob Allen <rob@akrabat.com>
2015-08-18 12:34:09 +0200

    Resolve callable classes
    Update CallableResolver to check if the string to resolve is in the
    container or a class that can be invoked.

--- Slim/CallableResolver.php
+++ Slim/CallableResolver.php
@@ -49,20 +49,33 @@ final class CallableResolver implements CallableResolverInterface
         if (!is_callable($toResolve) && is_string($toResolve)) {
             // check for slim callable as "class:method"
             $callablePattern = '!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
-            if (!preg_match($callablePattern, $toResolve, $matches)) {
-                throw new RuntimeException(sprintf('%s is not resolvable', $toResolve));
-            }
+            if (preg_match($callablePattern, $toResolve, $matches)) {
+                $class = $matches[1];
+                $method = $matches[2];
 
-            $class = $matches[1];
-            $method = $matches[2];
-
-            if ($this->container->has($class)) {
-                $resolved = [$this->container->get($class), $method];
-            } else {
-                if (!class_exists($class)) {
-                    throw new RuntimeException(sprintf('Callable %s does not exist', $class));
+                if ($this->container->has($class)) {
+                    $resolved = [$this->container->get($class), $method];
+                } else {
+                    if (!class_exists($class)) {
+                        throw new RuntimeException(sprintf('Callable %s does not exist', $class));
+                    }
+                    $resolved = [new $class, $method];
                 }
-                $resolved = [new $class, $method];
+            } else {
+                // check if string is something in the DIC that's callable or is a class name which
+                // has an __invoke() method
+                $class = $toResolve;
+                if ($this->container->has($class)) {
+                    $resolved = $this->container->get($class);
+                } else {
+                    if (!class_exists($class)) {
+                        throw new RuntimeException(sprintf('Callable %s does not exist', $class));
+                    }
+                    $resolved = new $class;
+                }
+            }
+            if (!is_callable($resolved)) {
+                throw new RuntimeException(sprintf('%s is not resolvable', $toResolve));
             }
         } else {
             $resolved = $toResolve;
