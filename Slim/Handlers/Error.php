1b7c8328700c45b017ad84349fe4d1800a72e37a
Mahmoud Adly <mahmoud.adly@gmail.com>
2015-08-29 20:35:35 +0200

    Add a meta tag for utf-8 content in error reporting.

--- Slim/Handlers/Error.php
+++ Slim/Handlers/Error.php
@@ -43,7 +43,8 @@ class Error
         }
 
         $output = sprintf(
-            "<html><head><title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana," .
+            "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>" .
+            "<title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana," .
             "sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;" .
             "width:65px;}</style></head><body><h1>%s</h1>%s</body></html>",
             $title,
