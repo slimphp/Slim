cedbd8eb8d217035aba6d689cdcde2aaf83f7d7e
Andrew Smith <a.smith@silentworks.co.uk>
2015-09-04 17:08:22 +0100

    Updated as per comment from @akrabat

--- Slim/Router.php
+++ Slim/Router.php
@@ -118,8 +118,9 @@ class Router extends RouteCollector implements RouterInterface
 
         // Prepend parent group pattern(s)
         if ($this->routeGroups) {
-            if (strlen($pattern) === 1 && strpos($pattern, '/') === 0) {
-                $pattern = ltrim($pattern, '/');
+            // If any route in the group only has / we remove it
+            if ($pattern === '/') {
+                $pattern = '';
             }
             $pattern = $this->processGroups() . $pattern;
         }
