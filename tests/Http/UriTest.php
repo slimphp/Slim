89c684d08178a0c6252c8f39475979763f62f994
Rob Allen <rob@akrabat.com>
2015-12-03 20:28:05 +0000

    Revert 1606 to allow routing of /index.php/foo again
    PR #1606 changed the way that the Uri's baseUrl was detected in order to
    fix #1590. However, this change meant that routing of URLs of the form
    /foo/index.php/bar/baz no longer worked.
    
    This commit reverts #1606 so that routing works again as the stripping
    off of the script name  when using rewriting could be done at the web
    server configuration level, or via a route the redirects to the one
    without the script name in it.

--- tests/Http/UriTest.php
+++ tests/Http/UriTest.php
@@ -616,8 +616,10 @@ class UriTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Handle case when the URL is /foo/index.php/bar/baz
-     * @ticket 1590
+     * When the URL is /foo/index.php/bar/baz, we need the baseURL to be
+     * /foo/index.php so that routing works correctly.
+     *
+     * @ticket 1639 as a fix to 1590 broke this.
      */
     public function testRequestURIContainsIndexDotPhp()
     {
@@ -629,6 +631,6 @@ class UriTest extends \PHPUnit_Framework_TestCase
                 ]
             )
         );
-        $this->assertSame('/foo', $uri->getBasePath());
+        $this->assertSame('/foo/index.php', $uri->getBasePath());
     }
 }
