4fea2cce75d49da11357b2f2473defc857cd2e12
Rob Allen <rob@akrabat.com>
2016-01-01 22:08:18 +0000

    Update copyright year to 2016

--- Slim/Router.php
+++ Slim/Router.php
@@ -3,7 +3,7 @@
  * Slim Framework (http://slimframework.com)
  *
  * @link      https://github.com/slimphp/Slim
- * @copyright Copyright (c) 2011-2015 Josh Lockhart
+ * @copyright Copyright (c) 2011-2016 Josh Lockhart
  * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
  */
 namespace Slim;
