22a2752e14c52d64e3c2b55a247a3e983e46f8bc
Rob Allen <rob@akrabat.com>
2016-02-27 15:05:23 +0000

    Silence deprecation notices in tests
    Use the silence operator over setExpectedException as HHVM doesn't pass
    otherwise.

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -47,7 +47,7 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetRequest()
     {
-        $this->assertInstanceOf('\Psr\Http\Message\RequestInterface', $this->container['request']);
+        @$this->assertInstanceOf('\Psr\Http\Message\RequestInterface', $this->container['request']);
     }
 
     /**
@@ -55,7 +55,7 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetResponse()
     {
-        $this->assertInstanceOf('\Psr\Http\Message\ResponseInterface', $this->container['response']);
+        @$this->assertInstanceOf('\Psr\Http\Message\ResponseInterface', $this->container['response']);
     }
 
     /**
