36cdaa2f4ec4495cfa3a8cb2b354bc2b862903d3
Julián Gutiérrez <j.gutierrez@gwella.es>
2016-03-29 12:04:02 +0200

    abstract handlers

--- Slim/Handlers/NotAllowed.php
+++ Slim/Handlers/NotAllowed.php
@@ -18,20 +18,8 @@ use Slim\Http\Body;
  * It outputs a simple message in either JSON, XML or HTML based on the
  * Accept header.
  */
-class NotAllowed
+class NotAllowed extends AbstractHandler
 {
-    /**
-     * Known handled content types
-     *
-     * @var array
-     */
-    protected $knownContentTypes = [
-        'application/json',
-        'application/xml',
-        'text/xml',
-        'text/html',
-    ];
-
     /**
      * Invoke error handler
      *
@@ -75,32 +63,6 @@ class NotAllowed
                 ->withHeader('Content-type', $contentType)
                 ->withHeader('Allow', $allow)
                 ->withBody($body);
-    }
-
-    /**
-     * Determine which content type we know about is wanted using Accept header
-     *
-     * @param ServerRequestInterface $request
-     * @return string
-     */
-    private function determineContentType(ServerRequestInterface $request)
-    {
-        $acceptHeader = $request->getHeaderLine('Accept');
-        $selectedContentTypes = array_intersect(explode(',', $acceptHeader), $this->knownContentTypes);
-
-        if (count($selectedContentTypes)) {
-            return $selectedContentTypes[0];
-        }
-
-        // handle +json and +xml specially
-        if (preg_match('/\+(json|xml)/', $acceptHeader, $matches)) {
-            $mediaType = 'application/' . $matches[1];
-            if (in_array($mediaType, $this->knownContentTypes)) {
-                return $mediaType;
-            }
-        }
-
-        return 'text/html';
     }
 
     /**
