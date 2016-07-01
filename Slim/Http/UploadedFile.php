157e16b82d84e1a45ef09a8bcfea58fbee2d718b
Jason Coward <jason@opengeek.com>
2016-07-01 16:36:49 -0600

    Do not check streams with is_writable in UploadedFile::moveTo()

--- Slim/Http/UploadedFile.php
+++ Slim/Http/UploadedFile.php
@@ -226,11 +226,11 @@ class UploadedFile implements UploadedFileInterface
             throw new RuntimeException('Uploaded file already moved');
         }
 
-        if (!is_writable(dirname($targetPath))) {
+        $targetIsStream = strpos($targetPath, '://') > 0;
+        if (!$targetIsStream && !is_writable(dirname($targetPath))) {
             throw new InvalidArgumentException('Upload target path is not writable');
         }
 
-        $targetIsStream = strpos($targetPath, '://') > 0;
         if ($targetIsStream) {
             if (!copy($this->file, $targetPath)) {
                 throw new RuntimeException(sprintf('Error moving uploaded file %1s to %2s', $this->name, $targetPath));
