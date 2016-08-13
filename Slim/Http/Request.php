030831938064572c4e4c24a89519aa30e8dfe164
Joel Huang <joelhy@gmail.com>
2016-08-13 11:25:37 +0800

    Add getServerParam method

--- Slim/Http/Request.php
+++ Slim/Http/Request.php
@@ -849,6 +849,22 @@ class Request extends Message implements ServerRequestInterface
         return $this->serverParams;
     }
 
+    /**
+     * Retrieve a server parameter.
+     *
+     * Note: This method is not part of the PSR-7 standard.
+     *
+     * @param  string $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function getServerParam($key, $default = null)
+    {
+        $serverParams = $this->getServerParams();
+
+        return isset($serverParams[$key]) ? $serverParams[$key] : $default;
+    }
+
     /*******************************************************************************
      * Attributes
      ******************************************************************************/
