8271f7adf216417f9123e5fc6110bc97c5b62dc1
tflight <tflight@gmail.com>
2016-10-18 18:26:52 -0400

    Update copyright year to 2016

--- Slim/Exception/ContainerException.php
+++ Slim/Exception/ContainerException.php
@@ -3,7 +3,7 @@
  * Slim Framework (http://slimframework.com)
  *
  * @link      https://github.com/codeguy/Slim
- * @copyright Copyright (c) 2011-2015 Josh Lockhart
+ * @copyright Copyright (c) 2011-2016 Josh Lockhart
  * @license   https://github.com/codeguy/Slim/blob/master/LICENSE (MIT License)
  */
 namespace Slim\Exception;
