c6869d7086fed0ecdb1c6defaec764cf70b71993
diegomariani <diego.mariani@mail.com>
2016-12-20 22:13:34 +0100

    refactoring

--- Slim/CallableResolver.php
+++ Slim/CallableResolver.php
@@ -18,6 +18,8 @@ use Slim\Interfaces\CallableResolverInterface;
  */
 final class CallableResolver implements CallableResolverInterface
 {
+    const CALLABLE_PATTERN = '!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
+
     /**
      * @var ContainerInterface
      */
@@ -46,45 +48,53 @@ final class CallableResolver implements CallableResolverInterface
      */
     public function resolve($toResolve)
     {
-        $resolved = $toResolve;
-
-        if (!is_callable($toResolve) && is_string($toResolve)) {
-            // check for slim callable as "class:method"
-            $callablePattern = '!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
-            if (preg_match($callablePattern, $toResolve, $matches)) {
-                $class = $matches[1];
-                $method = $matches[2];
-
-                if ($this->container->has($class)) {
-                    $resolved = [$this->container->get($class), $method];
-                } else {
-                    if (!class_exists($class)) {
-                        throw new RuntimeException(sprintf('Callable %s does not exist', $class));
-                    }
-                    $resolved = [new $class($this->container), $method];
-                }
-            } else {
-                // check if string is something in the DIC that's callable or is a class name which
-                // has an __invoke() method
-                $class = $toResolve;
-                if ($this->container->has($class)) {
-                    $resolved = $this->container->get($class);
-                } else {
-                    if (!class_exists($class)) {
-                        throw new RuntimeException(sprintf('Callable %s does not exist', $class));
-                    }
-                    $resolved = new $class($this->container);
-                }
-            }
+        if (is_callable($toResolve)) {
+            return $toResolve;
         }
 
-        if (!is_callable($resolved)) {
-            throw new RuntimeException(sprintf(
-                '%s is not resolvable',
-                is_array($toResolve) || is_object($toResolve) ? json_encode($toResolve) : $toResolve
-            ));
+        // check for slim callable as "class:method"
+        if (preg_match(self::CALLABLE_PATTERN, $toResolve, $matches)) {
+            $resolved = $this->resolveMethod($matches[1], $matches[2]);
         }
 
-        return $resolved;
+        $resolved = $this->resolveClass($toResolve);
+    }
+
+    /**
+     * @param string $class
+     * @param string $method
+     * @return callable
+     */
+    protected function resolveMethod($class, $method)
+    {
+        if ($this->container->has($class)) {
+            return [$this->container->get($class), $method];
+        }
+
+        if (!class_exists($class)) {
+            throw new RuntimeException(sprintf('Callable %s does not exist', $class));
+        }
+
+        return [new $class($this->container), $method];
+    }
+
+    /**
+     * Check if string is something in the DIC
+     * that's callable or is a class name which has an __invoke() method.
+     *
+     * @param string $class
+     * @return callable
+     */
+    protected function resolveClass($class)
+    {
+        if ($this->container->has($class)) {
+            return $this->container->get($class);
+        }
+
+        if (!class_exists($class)) {
+            throw new RuntimeException(sprintf('Callable %s does not exist', $class));
+        }
+
+        return new $class($this->container);
     }
 }
