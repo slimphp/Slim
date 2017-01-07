9eb68e734b7a5cb1008ff8a6a0c002061dc86b71
Rob Allen <rob@akrabat.com>
2017-01-07 12:21:11 +0000

    Update copyright year

--- tests/Middleware/SessionCookieTest.php
+++ tests/Middleware/SessionCookieTest.php
@@ -3,7 +3,7 @@
  * Slim - a micro PHP 5 framework
  *
  * @author      Josh Lockhart <info@slimframework.com>
- * @copyright   2011 Josh Lockhart
+ * @copyright   2011-2017 Josh Lockhart
  * @link        http://www.slimframework.com
  * @license     http://www.slimframework.com/license
  * @version     2.6.3
