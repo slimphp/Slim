f4d6f44e3a4ed7925de8a20572fb0bf4b96eb2d1
Ian Littman <iansltx@gmail.com>
2017-02-03 20:44:19 -0500

    Call not-found/bad-method handlers on invalid HTTP method
    Partially resolves #2005. Router map() calls don't have method checking,
    and we still don't link the valid methods in the request to valid
    methods at the application level (which IMO should be since the router
    doesn't enforce any sort of method limitation at this point), but now
    you won't get an uncaught exception when someone hits you with an HTTP
    message with verb CHICKEN.
    
    Also revised tests to check app run handling rather than request create
    handling, since internally to the request a bad method should still be
    invalid/throw an exception, so we don't want to weaken that guarantee.
    But we do want the application not to 500 at runtime.
    
    Along with that, behavior (as tested) differs between whether we have a
    route or not, since in any other case the former would give you a bad
    method exception, while the latter would give you a not-found. So these
    tweaks match up that behavior for obscure HTTP methods as well.
    
    Finally, RouterInterface now has constants for route info array returns.
    They're only used on the exceptional path, but they make it a bit
    clearer why we're pulling various numeric keys out of the route, rather
    than having a non-obvious contract with FastRoute.
    
    Took extra care here to respect order of execution for the existing code
    so anything relying on that order will still work. Likewise,
    InvalidMethodException subclasses InvalidArgumentException and returns
    the same message and code, so anything checking for exception strings
    etc., as well as instanceof checks, will still work.
    
    Credit to @nickdnk for building the first red test for this issue.

--- Slim/App.php
+++ Slim/App.php
@@ -9,6 +9,8 @@
 namespace Slim;
 
 use Exception;
+use Slim\Exception\InvalidMethodException;
+use Slim\Http\Response;
 use Throwable;
 use Closure;
 use InvalidArgumentException;
@@ -287,16 +289,54 @@ class App
      */
     public function run($silent = false)
     {
-        $request = $this->container->get('request');
+        try {
+            $request = $this->container->get('request');
+        } catch (InvalidMethodException $e) {
+            $request = $e->getRequest();
+        }
+
         $response = $this->container->get('response');
 
-        $response = $this->process($request, $response);
+        $response = !isset($e) ? $this->process($request, $response) : $this->processInvalidMethod($request, $response);
 
         if (!$silent) {
             $this->respond($response);
         }
 
         return $response;
+    }
+
+    /**
+     * Pull route info for a request with a bad method to decide whether to
+     * return a not-found error (default) or a bad-method error, then run
+     * the handler for that error, returning the resulting response.
+     *
+     * Used for cases where an incoming request has an unrecognized method,
+     * rather than throwing an exception and not catching it all the way up.
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function processInvalidMethod(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $router = $this->container->get('router');
+        if (is_callable([$request->getUri(), 'getBasePath']) && is_callable([$router, 'setBasePath'])) {
+            $router->setBasePath($request->getUri()->getBasePath());
+        }
+
+        $request = $this->dispatchRouterAndPrepareRoute($request, $router);
+        $routeInfo = $request->getAttribute('routeInfo', [RouterInterface::DISPATCH_STATUS => Dispatcher::NOT_FOUND]);
+
+        if ($routeInfo[RouterInterface::DISPATCH_STATUS] === Dispatcher::METHOD_NOT_ALLOWED) {
+            return $this->handleException(
+                new MethodNotAllowedException($request, $response, $routeInfo[RouterInterface::ALLOWED_METHODS]),
+                $request,
+                $response
+            );
+        }
+
+        return $this->handleException(new NotFoundException($request, $response), $request, $response);
     }
 
     /**
