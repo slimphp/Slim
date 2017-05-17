de333a6ae755dd73f6aac36d3753cd3779b713f2
Hannah Warmbier <hannahwarmbier@gmail.com>
2017-05-17 13:31:40 -0500

    Update docblock

--- Slim/RouteGroup.php
+++ Slim/RouteGroup.php
@@ -32,7 +32,7 @@ class RouteGroup extends Routable implements RouteGroupInterface
     /**
      * Invoke the group to register any Routable objects within it.
      *
-     * @param App $app The App to bind the callable to.
+     * @param App $app The App instance to pass to the group callable
      */
     public function __invoke(App $app = null)
     {
