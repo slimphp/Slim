3bb19aa491156f6f371cda9fb67170b0e461307b
Sam Burns <sam@sam-burns.com>
2017-05-25 16:27:29 +0100

    Minor PHPUnit tidy-up

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,13 +6,4 @@ date_default_timezone_set('America/New_York');
 // Prevent session cookies
 ini_set('session.use_cookies', 0);
 
-
-
-// Enable Composer autoloader
-/** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require dirname(__DIR__) . '/../vendor/autoload.php';
-
-require dirname(__FILE__) . '/getallheaders.php';
-
-// Register test classes
-$autoloader->addPsr4('Slim\Tests\\', __DIR__);
+require __DIR__ . '/getallheaders.php';
