d2a458ebb36d0fe825933a6fa827baceadbdb4de
Sam Burns <sam@sam-burns.com>
2017-05-25 17:37:48 +0100

    Adding basic Behat smoke test

--- tests/behat/context_files/Context.php
+++ tests/behat/context_files/Context.php
@@ -1,28 +1,91 @@
 <?php
 namespace Slim\Behat\Context;
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context as BehatContext;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Uri;
 
 class Context implements BehatContext
 {
     /**
-     * @Given xyz
+     * @var App
      */
-    public function xyz()
+    private $application;
+
+    /**
+     * @var ResponseInterface
+     */
+    private $responseRetrieved;
+
+    public function __construct()
     {
+        $this->application = new App();
     }
 
     /**
-     * @When abc
+     * @Given I have set up an application to return a response with Hello World
      */
-    public function abc()
+    public function iHaveSetUpAnApplicationToReturnAResponseWithHelloWorld()
     {
+        $this->application->get(
+            '/hello-world',
+            function (ServerRequestInterface $request, ResponseInterface $response) {
+                $response->getBody()->write('Hello world');
+                return $response;
+            }
+        );
     }
 
     /**
-     * @Then def
+     * @Given I have set up an application to echo Hello World
      */
-    public function def()
+    public function iHaveSetUpAnApplicationToEchoHelloWorld()
     {
+        $this->application->get(
+            '/hello-world',
+            function (ServerRequestInterface $request, ResponseInterface $response) {
+                echo 'Hello world';
+            }
+        );
+    }
+
+    /**
+     * @When I query the :uri route
+     */
+    public function iQueryTheRoute($uri)
+    {
+        $request = $this->getRequest();
+        $request = $request->withUri(Uri::createFromString('http://localhost/' . $uri))->withMethod('GET');
+        $this->responseRetrieved = $this->application->process($request, $this->getResponse());
+    }
+
+    /**
+     * @Then the response body should be :expectedResponseBody
+     */
+    public function theResponseBodyShouldBe($expectedResponseBody)
+    {
+        $responseBody = $this->responseRetrieved->getBody()->__toString();
+        \PHPUnit\Framework\Assert::assertEquals($expectedResponseBody, $responseBody);
+    }
+
+    /**
+     * @return Request
+     */
+    private function getRequest()
+    {
+        return $this->application->getContainer()->get('request');
+    }
+
+    /**
+     * @return Response
+     */
+    private function getResponse()
+    {
+        return $this->application->getContainer()->get('response');
     }
 }
--- tests/behat/feature_files/basic-feature.feature
+++
@@ -1,9 +0,0 @@
-Feature: Basic feature
-  As somebody
-  I want something
-  Because of reasons
-
-  Scenario: Basic scenario
-    Given xyz
-    When abc
-    Then def
