44065aef8150d2fb88260a3a417f9f7f575a8a6f
Sam Burns <sam@sam-burns.com>
2017-05-25 17:12:55 +0100

    Behat scaffolding

---
+++ tests/behat/context_files/Context.php
@@ -0,0 +1,28 @@
+<?php
+namespace Slim\Behat\Context;
+
+use Behat\Behat\Context\Context as BehatContext;
+
+class Context implements BehatContext
+{
+    /**
+     * @Given xyz
+     */
+    public function xyz()
+    {
+    }
+
+    /**
+     * @When abc
+     */
+    public function abc()
+    {
+    }
+
+    /**
+     * @Then def
+     */
+    public function def()
+    {
+    }
+}
---
+++ tests/behat/feature_files/basic-feature.feature
@@ -0,0 +1,9 @@
+Feature: Basic feature
+  As somebody
+  I want something
+  Because of reasons
+
+  Scenario: Basic scenario
+    Given xyz
+    When abc
+    Then def
