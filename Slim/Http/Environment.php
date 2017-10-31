63ba6881eb93da4847bdbfb9e150d35a2708dfa6
Juan Pablo Garcia <juanpgfe@gmail.com>
2017-10-31 19:44:14 -0400

    Code style fixed

--- Slim/Http/Environment.php
+++ Slim/Http/Environment.php
@@ -30,7 +30,7 @@ class Environment extends Collection implements EnvironmentInterface
     public static function mock(array $userData = [])
     {
         //Validates if default protocol is HTTPS to set default port 443
-        $defport = (isset($userData["HTTPS"]) || 
+        $defport = (isset($userData["HTTPS"]) ||
                     (isset($userData["SERVER_PROTOCOL"]) && substr($userData["SERVER_PROTOCOL"], 0, 5) === "HTTPS"))
                       ? 443 : 80;
 
