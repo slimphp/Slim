c7a13159ae42ecaa93a7d3881765bcf100609629
Daniel Opitz <git@copynpaste.de>
2017-11-05 01:21:14 +0100

    update/add links to license

--- Slim/Exception/InvalidMethodException.php
+++ Slim/Exception/InvalidMethodException.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2011-2017 Josh Lockhart
+ * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
+ */
 namespace Slim\Exception;
 
 use Psr\Http\Message\ServerRequestInterface;
