d575b9ab0113bf037c36f47e24248e8f4a83b9c1
Pierre Bérubé <pierre@lgse.com>
2018-11-18 23:42:28 -0700

    fix PHPStan error

--- Slim/Handlers/Strategies/RequestHandler.php
+++ Slim/Handlers/Strategies/RequestHandler.php
@@ -23,7 +23,7 @@ class RequestHandler implements InvocationStrategyInterface
     /**
      * Invoke a route callable that implments RequestHandlerInterface
      *
-     * @param callable         $callable
+     * @param callable               $callable
      * @param ServerRequestInterface $request
      * @param ResponseInterface      $response
      * @param array                  $routeArguments
