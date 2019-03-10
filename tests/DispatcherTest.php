1883f60f290f772ba1090b7c921907396f238069
l0gicgate <pierre@lgse.com>
2019-03-10 16:00:52 -0600

    remove dated copyright in all file headers

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @license https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+
 namespace Slim\Tests;
 
 use FastRoute\DataGenerator\GroupCountBased;
