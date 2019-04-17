dc801f5f4cb24812e195874eea2a352a28558396
Adrian Suter <adrian@suter-wirz.ch>
2019-04-17 17:14:03 +0200

    Correct syntax

--- Slim/Http/Headers.php
+++ Slim/Http/Headers.php
@@ -77,11 +77,7 @@ class Headers extends Collection implements HeadersInterface
     {
         $authorization = $environment->get('HTTP_AUTHORIZATION');
 
-        if (
-            empty($authorization) 
-            && is_callable('getallheaders') 
-            && $headers = getallheaders()
-        ) {
+        if (empty($authorization) && is_callable('getallheaders') && $headers = getallheaders()) {
             $headers = array_change_key_case($headers, CASE_LOWER);
             if (isset($headers['authorization'])) {
                 $environment->set('HTTP_AUTHORIZATION', $headers['authorization']);
