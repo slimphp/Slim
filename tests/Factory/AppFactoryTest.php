8c1ec1db656654291f8a1596ae70af2c225e51ca
l0gicgate <pierre@lgse.com>
2019-04-22 14:04:13 -0600

    add test coverage for new setters on AppFactory

--- tests/Factory/AppFactoryTest.php
+++ tests/Factory/AppFactoryTest.php
@@ -11,12 +11,17 @@ namespace Slim\Tests\Factory;
 
 use Http\Factory\Guzzle\ResponseFactory as GuzzleResponseFactory;
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use ReflectionProperty;
 use Slim\Factory\AppFactory;
 use Slim\Factory\Psr17\GuzzlePsr17Factory;
 use Slim\Factory\Psr17\NyholmPsr17Factory;
 use Slim\Factory\Psr17\SlimPsr17Factory;
 use Slim\Factory\Psr17\ZendDiactorosPsr17Factory;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\RouteCollectorInterface;
+use Slim\Interfaces\RouteResolverInterface;
 use Slim\Psr7\Factory\ResponseFactory as SlimResponseFactory;
 use Slim\Routing\RouteCollector;
 use Slim\Tests\TestCase;
@@ -36,14 +41,14 @@ class AppFactoryTest extends TestCase
 
     /**
      * @dataProvider provideImplementations
-     * @param string $implementation
+     * @param string $psr17factory
      * @param string $expectedResponseFactoryClass
      */
-    public function testCreateAppWithAllImplementations(string $implementation, string $expectedResponseFactoryClass)
+    public function testCreateAppWithAllImplementations(string $psr17factory, string $expectedResponseFactoryClass)
     {
-        $implementationsProperty = new ReflectionProperty(AppFactory::class, 'implementations');
-        $implementationsProperty->setAccessible(true);
-        $implementationsProperty->setValue([$implementation]);
+        $psr17FactoriesProperty = new ReflectionProperty(AppFactory::class, 'psr17Factories');
+        $psr17FactoriesProperty->setAccessible(true);
+        $psr17FactoriesProperty->setValue([$psr17factory]);
 
         $app = AppFactory::create();
 
@@ -62,10 +67,94 @@ class AppFactoryTest extends TestCase
      */
     public function testDetermineResponseFactoryThrowsRuntimeException()
     {
-        $implementationsProperty = new ReflectionProperty(AppFactory::class, 'implementations');
-        $implementationsProperty->setAccessible(true);
-        $implementationsProperty->setValue([]);
+        $psr17FactoriesProperty = new ReflectionProperty(AppFactory::class, 'psr17Factories');
+        $psr17FactoriesProperty->setAccessible(true);
+        $psr17FactoriesProperty->setValue([]);
 
         AppFactory::create();
+    }
+
+    public function testAppIsCreatedWithInstancesFromSetters()
+    {
+        $responseFactoryProphecy = $this->prophesize(ResponseFactoryInterface::class);
+        $containerProphecy = $this->prophesize(ContainerInterface::class);
+        $callableResolverProphecy = $this->prophesize(CallableResolverInterface::class);
+        $routeCollectorProphecy = $this->prophesize(RouteCollectorInterface::class);
+        $routeResolverProphecy = $this->prophesize(RouteResolverInterface::class);
+
+        AppFactory::setResponseFactory($responseFactoryProphecy->reveal());
+        AppFactory::setContainer($containerProphecy->reveal());
+        AppFactory::setCallableResolver($callableResolverProphecy->reveal());
+        AppFactory::setRouteCollector($routeCollectorProphecy->reveal());
+        AppFactory::setRouteResolver($routeResolverProphecy->reveal());
+
+        $app = AppFactory::create();
+
+        $this->assertSame(
+          $responseFactoryProphecy->reveal(),
+          $app->getResponseFactory()
+        );
+
+        $this->assertSame(
+            $containerProphecy->reveal(),
+            $app->getContainer()
+        );
+
+        $this->assertSame(
+            $callableResolverProphecy->reveal(),
+            $app->getCallableResolver()
+        );
+
+        $this->assertSame(
+            $routeCollectorProphecy->reveal(),
+            $app->getRouteCollector()
+        );
+
+        $this->assertSame(
+            $routeResolverProphecy->reveal(),
+            $app->getRouteResolver()
+        );
+    }
+
+    public function testAppIsCreatedWithInjectedInstancesFromFunctionArguments()
+    {
+        $responseFactoryProphecy = $this->prophesize(ResponseFactoryInterface::class);
+        $containerProphecy = $this->prophesize(ContainerInterface::class);
+        $callableResolverProphecy = $this->prophesize(CallableResolverInterface::class);
+        $routeCollectorProphecy = $this->prophesize(RouteCollectorInterface::class);
+        $routeResolverProphecy = $this->prophesize(RouteResolverInterface::class);
+
+        $app = AppFactory::create(
+            $responseFactoryProphecy->reveal(),
+            $containerProphecy->reveal(),
+            $callableResolverProphecy->reveal(),
+            $routeCollectorProphecy->reveal(),
+            $routeResolverProphecy->reveal()
+        );
+
+        $this->assertSame(
+            $responseFactoryProphecy->reveal(),
+            $app->getResponseFactory()
+        );
+
+        $this->assertSame(
+            $containerProphecy->reveal(),
+            $app->getContainer()
+        );
+
+        $this->assertSame(
+            $callableResolverProphecy->reveal(),
+            $app->getCallableResolver()
+        );
+
+        $this->assertSame(
+            $routeCollectorProphecy->reveal(),
+            $app->getRouteCollector()
+        );
+
+        $this->assertSame(
+            $routeResolverProphecy->reveal(),
+            $app->getRouteResolver()
+        );
     }
 }
