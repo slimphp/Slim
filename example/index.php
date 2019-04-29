d7d9f87209b2e193224160d52b19d051a33ef722
asaokamei <asao.kamei@gmail.com>
2019-04-29 20:00:20 +0900

    made example work.

--- example/index.php
+++ example/index.php
@@ -4,38 +4,44 @@
  * Require the Slim Framework using Composer's autoloader
  * This example uses: composer require slim/slim:4.x-dev slim/psr7:dev-master slim/http
  */
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 
 /**
  * Import relevant classes
  */
-use Slim\App;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\Factory\BuildContainer;
+use Slim\Factory\Builder;
+use Slim\Factory\ServiceProvider;
 use Slim\Http\Factory\DecoratedResponseFactory;
-use Slim\Http\ServerRequest;
 use Slim\Middleware\ErrorMiddleware;
 use Slim\Psr7\Factory\ResponseFactory;
-use Slim\Psr7\Factory\ServerRequestFactory;
 use Slim\Psr7\Factory\StreamFactory;
 
-/**
- * Create a PSR-17 response factory and instantiate our Slim appliciation ($app)
- */
-$responseFactory = new DecoratedResponseFactory(new ResponseFactory(), new StreamFactory());
-
-/**
- * Instantiate a Slim application
- *
- * This example instantiates a Slim application using its default settings. However, you may choose to configure
- * your Slim application now by passing an associative array of settings into the application constructor.
- */
-$app = new App($responseFactory);
+$provider = new ServiceProvider();
+$container = new BuildContainer($provider);
+$container->set(
+    ResponseFactoryInterface::class,
+    new DecoratedResponseFactory(new ResponseFactory(), new StreamFactory())
+);
+$builder = new Builder($container);
+$app = $builder->getApp();
 
 /**
  * Add middleware as required. This is a LIFO stack.
  * We recommend adding ErrorMiddleware at least
  */
-$app->add(new ErrorMiddleware($app->getCallableResolver(), $responseFactory, true, true, true));
+$app->add(
+    new ErrorMiddleware(
+        $app->getCallableResolver(),
+        $app->getResponseFactory(),
+        true,
+        true,
+        true
+    )
+);
 
 /**
  * Define the Slim application routes
@@ -61,5 +67,4 @@ $app->get('/hello[/{name}]', function ($request, $response, $args) {
  * This method should be called last. This executes the Slim application
  * and returns the HTTP response to the HTTP client.
  */
-$request = new ServerRequest(ServerRequestFactory::createFromGlobals());
-$app->run($request);
+$app->run();
