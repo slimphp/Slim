e87061ecf6c451934d9e83e160db4e6b6c4d6d57
l0gicgate <pierre@lgse.com>
2019-04-30 11:44:42 -0600

    move autoload-dev include files into tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,6 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/Assets/PhpFunctionOverrides.php';
+require __DIR__ . '/Assets/PhpRoutingFunctionOverrides.php';
