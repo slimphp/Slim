0a8db5144e57361ef930f28413e216d378bab205
Adrian Suter <adrian@suter-wirz.ch>
2019-06-10 01:03:51 +0200

    Fix code style

--- Slim/ResponseEmitter.php
+++ Slim/ResponseEmitter.php
@@ -129,7 +129,7 @@ class ResponseEmitter
      */
     public function isResponseEmpty(ResponseInterface $response): bool
     {
-        $contents = (string)$response->getBody();
+        $contents = (string) $response->getBody();
         return (empty($contents) && !is_numeric($contents))
             || in_array($response->getStatusCode(), [204, 205, 304], true);
     }
