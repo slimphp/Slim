d81255405ce1a2a6117ae809c36625b7c8cf9fb4
Rob Allen <rob@akrabat.com>
2019-08-13 14:06:00 +0100

    Sort use statements alphabetically in BodyParsingMiddleware

--- Slim/Middleware/BodyParsingMiddleware.php
+++ Slim/Middleware/BodyParsingMiddleware.php
@@ -9,11 +9,11 @@ declare(strict_types=1);
 
 namespace Slim\Middleware;
 
-use RuntimeException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
 
 class BodyParsingMiddleware implements MiddlewareInterface
 {
