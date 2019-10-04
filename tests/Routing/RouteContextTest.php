de91cf01c1e86da366772510806aaa7c2535c497
Adrian Suter <adrian@suter-wirz.ch>
2019-10-04 22:39:02 +0200

    Use RuntimeException

--- tests/Routing/RouteContextTest.php
+++ tests/Routing/RouteContextTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Slim\Tests\Routing;
 
 use Psr\Http\Message\ServerRequestInterface;
+use RuntimeException;
 use Slim\Interfaces\RouteInterface;
 use Slim\Interfaces\RouteParserInterface;
 use Slim\Routing\RouteContext;
