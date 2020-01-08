a768bec72cffaa9ac3bda67512f61549599318c5
Alex Barker <alex@1stleg.com>
2020-01-08 09:53:31 -0800

    Fixing more issues

--- Slim/CallableResolver.php
+++ Slim/CallableResolver.php
@@ -6,7 +6,7 @@
  * @license https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
  */
 
-declare(strict_types=0);
+declare(strict_types=1);
 
 namespace Slim;
 
@@ -56,7 +56,7 @@ final class CallableResolver implements AdvancedCallableResolverInterface
      */
     public function resolveRoute($toResolve): callable
     {
-        return $this->resolveSlimNotation($toResolve);
+        return $this->resolveSlimNotation($toResolve, 'handle');
     }
 
     /**
@@ -64,44 +64,34 @@ final class CallableResolver implements AdvancedCallableResolverInterface
      */
     public function resolveMiddleware($toResolve): callable
     {
-        return $this->resolveSlimNotation($toResolve);
+        return $this->resolveSlimNotation($toResolve, 'process');
     }
 
     /**
      * @param callable|string $toResolve
+     * @param string $defaultMethod
      * @return callable
      */
-    private function resolveSlimNotation($toResolve): callable
+    private function resolveSlimNotation($toResolve, string $defaultMethod = '__invoke'): callable
     {
         if (is_callable($toResolve)) {
             $resolved = $toResolve;
         } elseif (is_string($toResolve)) {
             if (preg_match(CallableResolver::$callablePattern, $toResolve, $matches)) {
-                $instance = $this->resolveClass($matches[1]);
-                $method = $matches[2];
+                $callable = [$this->resolveClass($matches[1]), $matches[2]];
             } else {
                 $instance = $this->resolveClass($toResolve);
-
-                if ($this->isRoute($instance)) {
-                    $method = 'handle';
-                } elseif ($this->isMiddleware($instance)) {
-                    $method = 'process';
+                if (is_callable($instance)) {
+                    $callable = $instance;
                 } else {
-                    $method = '__invoke';
+                    $callable = [$instance, $defaultMethod];
                 }
             }
 
-            $resolved = $this->assertCallable([$instance, $method], $toResolve);
+            $resolved = $this->assertCallable($callable, $toResolve);
         } else {
-            $instance = $toResolve;
-            if ($this->isRoute($instance)) {
-                $method = 'handle';
-            } elseif ($this->isMiddleware($instance)) {
-                $method = 'process';
-            } else {
-                $method = '__invoke';
-            }
-            $resolved = $this->assertCallable([$instance, $method], $toResolve);
+            $callable = [$toResolve, $defaultMethod];
+            $resolved = $this->assertCallable($callable, $toResolve);
         }
 
         return $this->bindToContainer($resolved);
