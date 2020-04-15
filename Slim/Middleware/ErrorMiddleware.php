220a81258e52bae5bb98df5e580172224c060abe
TiMESPLiNTER <github@timesplinter.ch>
2020-04-15 15:55:14 +0200

    Fix type hints

--- Slim/Middleware/ErrorMiddleware.php
+++ Slim/Middleware/ErrorMiddleware.php
@@ -132,7 +132,7 @@ class ErrorMiddleware implements MiddlewareInterface
      * occurs when processing the current request.
      *
      * @param string $type Exception/Throwable name. ie: RuntimeException::class
-     * @return string|callable|ErrorHandler
+     * @return callable|ErrorHandler
      */
     public function getErrorHandler(string $type)
     {
@@ -154,7 +154,7 @@ class ErrorMiddleware implements MiddlewareInterface
     /**
      * Get default error handler
      *
-     * @return ErrorHandler|callable|string
+     * @return ErrorHandler|callable
      */
     public function getDefaultErrorHandler()
     {
