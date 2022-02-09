6e69a2c58b8777d97ba305521eaec10304bb2ee2
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-09 12:00:10 +0300

    upgrade meta tags

--- Slim/Error/Renderers/HtmlErrorRenderer.php
+++ Slim/Error/Renderers/HtmlErrorRenderer.php
@@ -75,7 +75,8 @@ class HtmlErrorRenderer extends AbstractErrorRenderer
             '<!doctype html>' .
             '<html lang="en">' .
             '   <head>' .
-            "       <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>" .
+            '       <meta charset="utf-8">' .
+            '       <meta name="viewport" content="width=device-width, initial-scale=1">' .
             '       <title>%s</title>' .
             '       <style>' .
             '           body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif}' .
