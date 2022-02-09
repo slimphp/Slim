0882a5db99c516b8ee87a7e9758c0f091fb390bf
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-09 16:25:14 +0300

    re-indent with 4 spaces

--- Slim/Error/Renderers/HtmlErrorRenderer.php
+++ Slim/Error/Renderers/HtmlErrorRenderer.php
@@ -74,21 +74,21 @@ class HtmlErrorRenderer extends AbstractErrorRenderer
         return sprintf(
             '<!doctype html>' .
             '<html lang="en">' .
-            '   <head>' .
-            '       <meta charset="utf-8">' .
-            '       <meta name="viewport" content="width=device-width, initial-scale=1">' .
-            '       <title>%s</title>' .
-            '       <style>' .
-            '           body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif}' .
-            '           h1{margin:0;font-size:48px;font-weight:normal;line-height:48px}' .
-            '           strong{display:inline-block;width:65px}' .
-            '       </style>' .
-            '   </head>' .
-            '   <body>' .
-            '       <h1>%s</h1>' .
-            '       <div>%s</div>' .
-            '       <a href="#" onclick="window.history.go(-1)">Go Back</a>' .
-            '   </body>' .
+            '    <head>' .
+            '        <meta charset="utf-8">' .
+            '        <meta name="viewport" content="width=device-width, initial-scale=1">' .
+            '        <title>%s</title>' .
+            '        <style>' .
+            '            body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif}' .
+            '            h1{margin:0;font-size:48px;font-weight:normal;line-height:48px}' .
+            '            strong{display:inline-block;width:65px}' .
+            '        </style>' .
+            '    </head>' .
+            '    <body>' .
+            '        <h1>%s</h1>' .
+            '        <div>%s</div>' .
+            '        <a href="#" onclick="window.history.go(-1)">Go Back</a>' .
+            '    </body>' .
             '</html>',
             $title,
             $title,
