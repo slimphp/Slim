36992312b3e5ee8b7ed0a64cf7022ec20983772f
Pierre Bérubé <pierre@lgse.com>
2022-03-14 08:16:43 -0600

    update app version to 4.10.0

--- Slim/App.php
+++ Slim/App.php
@@ -38,7 +38,7 @@ class App extends RouteCollectorProxy implements RequestHandlerInterface
      *
      * @var string
      */
-    public const VERSION = '4.9.0';
+    public const VERSION = '4.10.0';
 
     /**
      * @var RouteResolverInterface
