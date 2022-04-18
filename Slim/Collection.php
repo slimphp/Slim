a4a4fbb5e316ea1a55321bbe92621f9e56f59dab
Alex Ermashev <alex.ermashev@carrentalgateway.com>
2022-04-18 16:48:13 +0600

    fix php 8 compatibility

--- Slim/Collection.php
+++ Slim/Collection.php
@@ -109,7 +109,7 @@ class Collection implements CollectionInterface
      *
      * @return bool
      */
-    public function offsetExists($key)
+    public function offsetExists($key): bool
     {
         return $this->has($key);
     }
@@ -121,7 +121,7 @@ class Collection implements CollectionInterface
      *
      * @return mixed The key's value, or the default value
      */
-    public function offsetGet($key)
+    public function offsetGet($key): mixed
     {
         return $this->get($key);
     }
@@ -132,7 +132,7 @@ class Collection implements CollectionInterface
      * @param string $key   The data key
      * @param mixed  $value The data value
      */
-    public function offsetSet($key, $value)
+    public function offsetSet($key, $value): void
     {
         $this->set($key, $value);
     }
@@ -142,7 +142,7 @@ class Collection implements CollectionInterface
      *
      * @param string $key The data key
      */
-    public function offsetUnset($key)
+    public function offsetUnset($key): void
     {
         $this->remove($key);
     }
@@ -152,7 +152,7 @@ class Collection implements CollectionInterface
      *
      * @return int
      */
-    public function count()
+    public function count(): int
     {
         return count($this->data);
     }
@@ -162,7 +162,7 @@ class Collection implements CollectionInterface
      *
      * @return ArrayIterator
      */
-    public function getIterator()
+    public function getIterator(): ArrayIterator
     {
         return new ArrayIterator($this->data);
     }
