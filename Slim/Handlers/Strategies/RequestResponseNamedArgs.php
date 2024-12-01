b959895fe0aec8b94e9ba5cf73e62dd080a01261
Daniel Opitz <d.opitz@outlook.com>
2024-12-01 17:20:23 +0100

    Remove PHP version check

--- Slim/Handlers/Strategies/RequestResponseNamedArgs.php
+++ Slim/Handlers/Strategies/RequestResponseNamedArgs.php
@@ -21,13 +21,6 @@ use RuntimeException;
  */
 class RequestResponseNamedArgs implements InvocationStrategyInterface
 {
-    public function __construct()
-    {
-        if (PHP_VERSION_ID < 80000) {
-            throw new RuntimeException('Named arguments are only available for PHP >= 8.0.0');
-        }
-    }
-
     /**
      * Invoke a route callable with request, response and all route parameters
      * as individual arguments.
