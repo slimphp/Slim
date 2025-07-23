f0fdcd9d94ae799da031785e76646d96146b2667
Junior <gjunior@paytech.com.br>
2025-07-23 10:05:03 -0300

    Flash.php - explicity return types
    Added explicit return types in methods that implements interfaces

--- Slim/Middleware/Flash.php
+++ Slim/Middleware/Flash.php
@@ -6,7 +6,7 @@
  * @copyright   2011-2017 Josh Lockhart
  * @link        http://www.slimframework.com
  * @license     http://www.slimframework.com/license
- * @version     2.6.4
+ * @version     2.6.3
  * @package     Slim
  *
  * MIT LICENSE
@@ -155,7 +155,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
     /**
      * Array Access: Offset Exists
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         $messages = $this->getMessages();
 
@@ -165,7 +165,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
     /**
      * Array Access: Offset Get
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         $messages = $this->getMessages();
 
@@ -175,7 +175,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
     /**
      * Array Access: Offset Set
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         $this->now($offset, $value);
     }
@@ -183,7 +183,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
     /**
      * Array Access: Offset Unset
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->messages['prev'][$offset], $this->messages['now'][$offset]);
     }
@@ -192,7 +192,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
      * Iterator Aggregate: Get Iterator
      * @return \ArrayIterator
      */
-    public function getIterator()
+    public function getIterator(): \ArrayIterator
     {
         $messages = $this->getMessages();
 
@@ -202,7 +202,7 @@ class Flash extends \Slim\Middleware implements \ArrayAccess, \IteratorAggregate
     /**
      * Countable: Count
      */
-    public function count()
+    public function count(): int
     {
         return count($this->getMessages());
     }
